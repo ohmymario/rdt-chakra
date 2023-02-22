b8251300387961fa8fa0002065d80ac0ac4b620f
Mario Ballesteros <mariob360@gmail.com>
2023-02-22 12:05:45 -0800

    wrap app in layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,15 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "../chakra/theme";
+import Layout from "@/component/Layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     // Provides the Chakra theme to all components
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
