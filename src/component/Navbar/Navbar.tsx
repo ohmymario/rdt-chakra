83b13e975f805bdcafb7f7d5ebdc072a474ce835
Mario Ballesteros <mariob360@gmail.com>
2023-02-22 13:42:14 -0800

    add navbar logo

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import { FunctionComponent } from "react";
 
 interface NavbarProps {}
@@ -6,7 +6,28 @@ interface NavbarProps {}
 const Navbar: FunctionComponent<NavbarProps> = (props: NavbarProps) => {
   return (
     <Flex bg="white" height="44px" padding="6px 12px" border="1px solid red">
-      <Flex>Logos</Flex>
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" alt="logo" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          alt="logo"
+          height="40px"
+          // media query
+          // mobile first
+          display={{
+            base: "none",
+            md: "unset",
+          }}
+        />
+      </Flex>
+      {/* Show Directory if Logged In */}
+      {/* <Directory /> */}
+
+      {/* Always Shown */}
+      {/* <SearchInput /> */}
+
+      {/* Show RightContent if Logged In */}
+      {/* <RightContent /> */}
     </Flex>
   );
 };
