50da0bee4c39805e53487500c81f293fade4bb44
Mario Ballesteros <mariob360@gmail.com>
2023-02-21 23:47:20 -0800

    add custom bg

--- src/chakra/theme.tsx
+++ src/chakra/theme.tsx
@@ -1,5 +1,7 @@
 import { extendTheme } from "@chakra-ui/react";
-import "@fontsource/open-sans";
+import "@fontsource/ibm-plex-sans/300.css";
+import "@fontsource/ibm-plex-sans/400.css";
+import "@fontsource/ibm-plex-sans/700.css";
 
 export const theme = extendTheme({
   colors: {
@@ -9,5 +11,25 @@ export const theme = extendTheme({
   },
   fonts: {
     body: "Open Sans, sans-serif",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.200",
+      },
+    },
+  },
+  component: {
+    Button: {
+      baseStyle: {
+        fontWeight: "bold",
+      },
+      variants: {
+        "with-shadow": {
+          bg: "red.400",
+          boxShadow: "0 0 2px 2px #efdfde",
+        },
+      },
+    },
   },
 });
