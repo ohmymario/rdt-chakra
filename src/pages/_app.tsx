9a3d0d1d4631bcd2707324372359b14d0149c3db
Mario Ballesteros <mariob360@gmail.com>
2023-02-23 14:57:27 -0800

    add the recoil provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,20 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "../chakra/theme";
 import Layout from "@/component/Layout/Layout";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    // Provides the Chakra theme to all components
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <>
+      {/* Provides the Recoil state to all componentss */}
+      <RecoilRoot>
+        {/* Provides the Chakra theme to all components */}
+        <ChakraProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </RecoilRoot>
+    </>
   );
 }
