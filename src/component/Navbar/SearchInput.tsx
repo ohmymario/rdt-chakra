4593a7801b241b89ce9facf9c43176055cc349b2
Mario Ballesteros <mariob360@gmail.com>
2023-02-22 17:49:29 -0800

    create searchinput component

---
+++ src/component/Navbar/SearchInput.tsx
@@ -0,0 +1,44 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { FunctionComponent } from "react";
+
+interface SearchInputProps {
+  // user: User;
+}
+
+const SearchInput: FunctionComponent<SearchInputProps> = (
+  props: SearchInputProps
+) => {
+  return (
+    <Flex flexGrow={1} mr={2} alignItems="center">
+      <InputGroup>
+        <InputLeftElement
+          pointerEvents="none"
+          height="100%"
+          children={<SearchIcon boxSize={"5"} color="gray.500" mt="1px" />}
+        />
+        <Input
+          placeholder="Search Reddit"
+          fontSize="10pt"
+          height="34px"
+          bg={"gray.50"}
+          _placeholder={{
+            color: "gray.500",
+          }}
+          _hover={{
+            bg: "white",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+          _focus={{
+            outline: "none",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+        />
+      </InputGroup>
+    </Flex>
+  );
+};
+
+export default SearchInput;
