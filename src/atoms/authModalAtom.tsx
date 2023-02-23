ab92514879c64368710e87ff673632c1b1aa01e9
Mario Ballesteros <mariob360@gmail.com>
2023-02-23 14:58:25 -0800

    create recoil state file

---
+++ src/atoms/authModalAtom.tsx
@@ -0,0 +1,19 @@
+import { atom } from "recoil";
+
+// typescript
+export interface AuthModalState {
+  isOpen: boolean;
+  view: "login" | "signup" | "resetPassword";
+}
+
+// default state
+const defaultModalState: AuthModalState = {
+  isOpen: false,
+  view: "login",
+};
+
+// atom state to give to recoil
+export const authModalState = atom<AuthModalState>({
+  key: "authModalState",
+  default: defaultModalState,
+});
