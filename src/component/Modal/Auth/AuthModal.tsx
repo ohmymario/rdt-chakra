6875c6f7475fa7e1cdc93ad57fae16e5eed74058
Mario Ballesteros <mariob360@gmail.com>
2023-02-22 18:13:12 -0800

    create barebones modal

---
+++ src/component/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,48 @@
+import {
+  useDisclosure,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from "@chakra-ui/react";
+import { FunctionComponent } from "react";
+
+interface AuthModalProps {}
+
+const AuthModal: FunctionComponent<AuthModalProps> = (
+  props: AuthModalProps
+) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+            distinctio ipsa, fuga quidem, voluptatum, similique molestias maxime
+            sit a explicabo reiciendis ducimus fugiat ullam temporibus rerum
+            assumenda iure unde. Quasi.
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default AuthModal;
