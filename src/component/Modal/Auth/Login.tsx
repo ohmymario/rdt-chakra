3cf8178c43a8d143938919f0b6f12eb3ca4e93d0
Mario Ballesteros <mariob360@gmail.com>
2023-02-23 18:03:59 -0800

    move styles in login to seperate variable

--- src/component/Modal/Auth/Login.tsx
+++ src/component/Modal/Auth/Login.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Input, VStack } from "@chakra-ui/react";
+import { Box, Button, Input, VStack } from "@chakra-ui/react";
 import { FunctionComponent, useState } from "react";
 
 interface LoginProps {}
@@ -21,8 +21,27 @@ const Login: FunctionComponent<LoginProps> = (props: LoginProps) => {
     console.log(`🚀 ~ handeSubmit ~ e:`, e);
   };
 
+  const inputStyles = {
+    fontSize: "10pt",
+    _placeholder: {
+      color: "gray.500",
+    },
+    _hover: {
+      bg: "white",
+      border: "1px solid",
+      borderColor: "blue.500",
+    },
+    _focus: {
+      outline: "white",
+      bg: "white",
+      border: "1px solid",
+      borderColor: "blue.500",
+    },
+    bg: "gray.50",
+  };
+
   return (
-    <Box mb={4} width="100%" border={"1px solid red"}>
+    <Box mb={4} width="100%">
       <form onSubmit={(e) => handeSubmit(e)}>
         <VStack spacing={4} align="stretch">
           <Input
@@ -30,12 +49,14 @@ const Login: FunctionComponent<LoginProps> = (props: LoginProps) => {
             type="email"
             placeholder="Email"
             onChange={(e) => handleLoginForm(e)}
+            {...inputStyles}
           />
           <Input
             name="password"
             type="password"
             placeholder="Password"
             onChange={(e) => handleLoginForm(e)}
+            {...inputStyles}
           />
 
           <Button height="36px" type="submit">
