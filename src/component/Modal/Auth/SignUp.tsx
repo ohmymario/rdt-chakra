13163f02b4596d2b7932e11152f9a820237ec075
Mario Ballesteros <mariob360@gmail.com>
2023-02-24 20:45:18 -0800

    update signup component to single quotes

--- src/component/Modal/Auth/SignUp.tsx
+++ src/component/Modal/Auth/SignUp.tsx
@@ -1,7 +1,7 @@
-import { authModalState } from "@/atoms/authModalAtom";
-import { Box, Button, Flex, Input, Text, VStack } from "@chakra-ui/react";
-import { FunctionComponent, useState } from "react";
-import { useSetRecoilState } from "recoil";
+import { authModalState } from '@/atoms/authModalAtom';
+import { Box, Button, Flex, Input, Text, VStack } from '@chakra-ui/react';
+import { FunctionComponent, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
 
 interface SignUpProps {}
 
@@ -9,9 +9,9 @@ const SignUp: FunctionComponent<SignUpProps> = (props: SignUpProps) => {
   const setAuthModalState = useSetRecoilState(authModalState);
 
   const [SignupForm, setSignupForm] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
+    email: '',
+    password: '',
+    confirmPassword: '',
   });
 
   const handleSignupForm = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,65 +26,65 @@ const SignUp: FunctionComponent<SignUpProps> = (props: SignUpProps) => {
   };
 
   const inputStyles = {
-    fontSize: "10pt",
+    fontSize: '10pt',
     _placeholder: {
-      color: "gray.500",
+      color: 'gray.500',
     },
     _hover: {
-      bg: "white",
-      border: "1px solid",
-      borderColor: "blue.500",
+      bg: 'white',
+      border: '1px solid',
+      borderColor: 'blue.500',
     },
     _focus: {
-      outline: "white",
-      bg: "white",
-      border: "1px solid",
-      borderColor: "blue.500",
+      outline: 'white',
+      bg: 'white',
+      border: '1px solid',
+      borderColor: 'blue.500',
     },
-    bg: "gray.50",
+    bg: 'gray.50',
   };
 
   return (
-    <Box mb={4} width="100%">
+    <Box mb={4} width='100%'>
       <form onSubmit={(e) => handleSubmit(e)}>
-        <VStack spacing={4} align="stretch">
+        <VStack spacing={4} align='stretch'>
           <Input
-            name="email"
-            type="email"
-            placeholder="Email"
+            name='email'
+            type='email'
+            placeholder='Email'
             onChange={(e) => handleSignupForm(e)}
             {...inputStyles}
           />
           <Input
-            name="password"
-            type="password"
-            placeholder="Password"
+            name='password'
+            type='password'
+            placeholder='Password'
             onChange={(e) => handleSignupForm(e)}
             {...inputStyles}
           />
           <Input
-            name="confirmPassword"
-            type="password"
-            placeholder="Confirm Password"
+            name='confirmPassword'
+            type='password'
+            placeholder='Confirm Password'
             onChange={(e) => handleSignupForm(e)}
             {...inputStyles}
           />
 
-          <Button height="36px" type="submit">
+          <Button height='36px' variant='auth' type='submit'>
             Sign Up
           </Button>
 
-          <Flex fontSize="10pt">
+          <Flex fontSize='10pt'>
             <Text mr={1}>Already a redditor?</Text>
             <Text
-              color="blue.500"
+              color='blue.500'
               fontWeight={700}
-              cursor="pointer"
-              textDecoration={"underline"}
+              cursor='pointer'
+              textDecoration={'underline'}
               onClick={() =>
                 setAuthModalState({
                   isOpen: true,
-                  view: "login",
+                  view: 'login',
                 })
               }
             >
