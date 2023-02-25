7465ec0eec05f477689013c0be7b84e1960ede18
Mario Ballesteros <mariob360@gmail.com>
2023-02-24 20:45:31 -0800

    update login component to single quotes

--- src/component/Modal/Auth/Login.tsx
+++ src/component/Modal/Auth/Login.tsx
@@ -1,7 +1,7 @@
-import { authModalState } from "@/atoms/authModalAtom";
-import { Box, Button, Flex, Input, Text, VStack } from "@chakra-ui/react";
-import { FunctionComponent, useState } from "react";
-import { useSetRecoilState } from "recoil";
+import { authModalState } from '@/atoms/authModalAtom';
+import { Box, Button, Flex, Input, Text, VStack } from '@chakra-ui/react';
+import { FunctionComponent, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
 
 interface LoginProps {}
 
@@ -9,8 +9,8 @@ const Login: FunctionComponent<LoginProps> = (props: LoginProps) => {
   const setAuthModalState = useSetRecoilState(authModalState);
 
   const [LoginForm, setLoginForm] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
 
   const handleLoginForm = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -25,58 +25,58 @@ const Login: FunctionComponent<LoginProps> = (props: LoginProps) => {
   };
 
   const inputStyles = {
-    fontSize: "10pt",
+    fontSize: '10pt',
     _placeholder: {
-      color: "gray.500",
+      color: 'gray.500',
     },
     _hover: {
-      bg: "white",
-      border: "1px solid",
-      borderColor: "blue.500",
+      bg: 'white',
+      border: '1px solid',
+      borderColor: 'blue.500',
     },
     _focus: {
-      outline: "white",
-      bg: "white",
-      border: "1px solid",
-      borderColor: "blue.500",
+      outline: 'white',
+      bg: 'white',
+      border: '1px solid',
+      borderColor: 'blue.500',
     },
-    bg: "gray.50",
+    bg: 'gray.50',
   };
 
   return (
-    <Box mb={4} width="100%">
+    <Box mb={4} width='100%'>
       <form onSubmit={(e) => handleSubmit(e)}>
-        <VStack spacing={4} align="stretch">
+        <VStack spacing={4} align='stretch'>
           <Input
-            name="email"
-            type="email"
-            placeholder="Email"
+            name='email'
+            type='email'
+            placeholder='Email'
             onChange={(e) => handleLoginForm(e)}
             {...inputStyles}
           />
           <Input
-            name="password"
-            type="password"
-            placeholder="Password"
+            name='password'
+            type='password'
+            placeholder='Password'
             onChange={(e) => handleLoginForm(e)}
             {...inputStyles}
           />
 
-          <Button height="36px" type="submit">
+          <Button height='36px' variant={'auth'} type='submit'>
             Log In
           </Button>
 
-          <Flex fontSize="10pt">
+          <Flex fontSize='10pt'>
             <Text mr={1}>New Here?</Text>
             <Text
-              color="blue.500"
+              color='blue.500'
               fontWeight={700}
-              cursor="pointer"
-              textDecoration={"underline"}
+              cursor='pointer'
+              textDecoration={'underline'}
               onClick={() =>
                 setAuthModalState({
                   isOpen: true,
-                  view: "signup",
+                  view: 'signup',
                 })
               }
             >
