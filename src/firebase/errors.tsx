c22c743278585481ec69d06e4ed32e018773a62e
Mario Ballesteros <mariob360@gmail.com>
2023-02-28 15:49:25 -0800

    add more firebase errors

--- src/firebase/errors.tsx
+++ src/firebase/errors.tsx
@@ -1,7 +1,21 @@
 export const FIREBASE_ERRORS = {
   'Firebase: Error (auth/email-already-in-use).':
     'A user with that email already exists',
-
   'Firebase: Error (auth/user-not-found).': 'Invalid email or password',
   'Firebase: Error (auth/wrong-password).': 'Invalid email or password',
+  'Firebase: Password should be at least 6 characters (auth/weak-password).':
+    'Password should be at least 6 characters',
+  'Firebase: Error (auth/invalid-email).': 'Invalid email',
+  'Firebase: Error (auth/operation-not-allowed).':
+    'This operation is not allowed',
+  'Firebase: Error (auth/invalid-credential).': 'Invalid credential',
+  'Firebase: Error (auth/user-disabled).': 'This user has been disabled',
+  'Firebase: Error (auth/invalid-verification-code).':
+    'Invalid verification code',
+  'Firebase: Error (auth/invalid-verification-id).': 'Invalid verification id',
+  'Firebase: Error (auth/missing-verification-code).':
+    'Missing verification code',
+  'Firebase: Error (auth/missing-verification-id).': 'Missing verification id',
+  'Firebase: Error (auth/credential-already-in-use).':
+    'This credential is already in use',
 };
