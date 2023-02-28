e10b88b60b4b87f653328aaaa7566b3f375ffd3b
Mario Ballesteros <mariob360@gmail.com>
2023-02-28 14:40:37 -0800

    update authmodalatom to use single quotes

--- src/atoms/authModalAtom.tsx
+++ src/atoms/authModalAtom.tsx
@@ -1,19 +1,20 @@
-import { atom } from "recoil";
+import { atom } from 'recoil';
 
 // typescript
+
 export interface AuthModalState {
   isOpen: boolean;
-  view: "login" | "signup" | "resetPassword";
+  view: 'login' | 'signup' | 'resetPassword';
 }
 
 // default state
 const defaultModalState: AuthModalState = {
   isOpen: false,
-  view: "login",
+  view: 'login',
 };
 
 // atom state to give to recoil
 export const authModalState = atom<AuthModalState>({
-  key: "authModalState",
+  key: 'authModalState',
   default: defaultModalState,
 });
