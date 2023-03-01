ae26e4365f26f120421d2cebec0dcec5cfa5efba
Mario Ballesteros <mariob360@gmail.com>
2023-03-01 13:32:56 -0800

    add user access to authmodal

--- src/component/Modal/Auth/AuthModal.tsx
+++ src/component/Modal/Auth/AuthModal.tsx
@@ -1,4 +1,4 @@
-import { authModalState } from "@/atoms/authModalAtom";
+import { authModalState } from '@/atoms/authModalAtom';
 import {
   Flex,
   Modal,
@@ -9,12 +9,15 @@ import {
   ModalOverlay,
   Text,
   VStack,
-} from "@chakra-ui/react";
-import { FunctionComponent } from "react";
+} from '@chakra-ui/react';
+import { FunctionComponent, useEffect } from 'react';
 
-import { useRecoilState } from "recoil";
-import AuthInputs from "./AuthInputs";
-import OAuthButtons from "./OAuthButtons";
+import { useRecoilState } from 'recoil';
+import AuthInputs from './AuthInputs';
+import OAuthButtons from './OAuthButtons';
+
+import { auth } from '@/firebase/clientApp';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 interface AuthModalProps {}
 
@@ -23,38 +26,50 @@ const AuthModal: FunctionComponent<AuthModalProps> = (
 ) => {
   const [modalState, setModalState] = useRecoilState(authModalState);
 
+  // triggered when user is logged in / logged out
+  const [user, loading, error] = useAuthState(auth);
+  // user: The auth.UserCredential if logged in | null
+  // loading: boolean indicate whether the authentication state is still being loaded
+  // error: Any AuthError by Firebase when trying to load the user, or undefined if there is no error
+
   const handleClose = () => {
     setModalState({ ...modalState, isOpen: false });
   };
+
+  useEffect(() => {
+    if (user) {
+      console.log(`🚀 ~ useEffect ~ user:`, user);
+    }
+  }, [user]);
 
   return (
     <>
       <Modal isOpen={modalState.isOpen} onClose={() => handleClose()}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader textAlign="center">
-            {modalState.view === "login" && "Login"}
-            {modalState.view === "signup" && "Sign Up"}
-            {modalState.view === "resetPassword" && "Reset Password"}
+          <ModalHeader textAlign='center'>
+            {modalState.view === 'login' && 'Login'}
+            {modalState.view === 'signup' && 'Sign Up'}
+            {modalState.view === 'resetPassword' && 'Reset Password'}
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
+            display='flex'
+            flexDirection='column'
+            alignItems='center'
+            justifyContent='center'
           >
             <VStack
-              display="flex"
-              direction="column"
-              alignItems="center"
-              justifyContent="center"
-              width="70%"
+              display='flex'
+              direction='column'
+              alignItems='center'
+              justifyContent='center'
+              width='70%'
               spacing={4}
             >
               <OAuthButtons />
 
-              <Text color="gray.500" fontWeight="700" textTransform="uppercase">
+              <Text color='gray.500' fontWeight='700' textTransform='uppercase'>
                 Or
               </Text>
 
