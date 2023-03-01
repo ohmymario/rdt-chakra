21a6e5ac817bcf5531bfd9c9b4fe39759a3a2c11
Mario Ballesteros <mariob360@gmail.com>
2023-03-01 15:07:21 -0800

    add signout in rightcontent component

--- src/component/Navbar/RightContent/RightContent.tsx
+++ src/component/Navbar/RightContent/RightContent.tsx
@@ -1,17 +1,41 @@
-import AuthModal from "@/component/Modal/Auth/AuthModal";
-import { Flex } from "@chakra-ui/react";
-import { FunctionComponent } from "react";
-import AuthButtons from "./AuthButtons";
+import AuthModal from '@/component/Modal/Auth/AuthModal';
+import { Button, Flex, Text } from '@chakra-ui/react';
+import { FunctionComponent } from 'react';
+import AuthButtons from './AuthButtons';
 
-interface RightContentProps {}
+import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
+
+import { User as FirebaseUser } from 'firebase/auth';
+
+interface RightContentProps {
+  user: FirebaseUser | null | undefined;
+}
 
 const RightContent: FunctionComponent<RightContentProps> = (
   props: RightContentProps
 ) => {
+  const [user, loading, error] = useAuthState(auth);
+  const [signOut] = useSignOut(auth);
+
   return (
     <>
-      <Flex as="nav" justifyContent="center" alignItems="center">
-        <AuthButtons />
+      <Flex as='nav' justifyContent='center' alignItems='center'>
+        {user ? (
+          <Button
+            height={8}
+            onClick={async () => {
+              const success = await signOut();
+              if (success) {
+                console.log('Sign out success');
+              }
+            }}
+          >
+            Sign out
+          </Button>
+        ) : (
+          <AuthButtons />
+        )}
       </Flex>
       <AuthModal />
     </>
