a3b7e9c52c17db848930b07a02ee600af00769ab
Mario Ballesteros <mariob360@gmail.com>
2023-03-01 13:34:41 -0800

    give user access to navbar/pass into rightcontent

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,35 +1,33 @@
-import { Flex, Image } from "@chakra-ui/react";
-import { FunctionComponent } from "react";
-import RightContent from "./RightContent/RightContent";
-import SearchInput from "./SearchInput";
+import { auth } from '@/firebase/clientApp';
+import { Flex, Image } from '@chakra-ui/react';
+import { FunctionComponent } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import RightContent from './RightContent/RightContent';
+import SearchInput from './SearchInput';
 
 interface NavbarProps {}
 
 const Navbar: FunctionComponent<NavbarProps> = (props: NavbarProps) => {
+  const [user, loading, error] = useAuthState(auth);
+
   return (
-    <Flex as="header" bg="white" height="44px" padding="6px 12px">
-      <Flex align="center">
-        <Image src="/images/redditFace.svg" alt="logo" height="30px" />
+    <Flex as='header' bg='white' height='44px' padding='6px 12px'>
+      <Flex align='center'>
+        <Image src='/images/redditFace.svg' alt='logo' height='30px' />
         <Image
-          src="/images/redditText.svg"
-          alt="logo"
-          height="40px"
-          // media query
-          // mobile first
+          src='/images/redditText.svg'
+          alt='logo'
+          height='40px'
           display={{
-            base: "none",
-            md: "unset",
+            base: 'none',
+            md: 'unset',
           }}
         />
       </Flex>
-      {/* Show Directory if Logged In */}
       {/* <Directory /> */}
 
-      {/* Always Shown */}
       <SearchInput />
-
-      {/* Show RightContent if Logged In */}
-      <RightContent />
+      <RightContent user={user} />
     </Flex>
   );
 };
