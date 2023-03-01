cca54e996f02f26e703948fdf2370fa56d11ec84
Mario Ballesteros <mariob360@gmail.com>
2023-03-01 00:07:59 -0800

    add several more friendly error messages

--- src/firebase/errors.tsx
+++ src/firebase/errors.tsx
@@ -1,21 +1,24 @@
+// prettier-ignore
 export const FIREBASE_ERRORS = {
-  'Firebase: Error (auth/email-already-in-use).':
-    'A user with that email already exists',
+  'Firebase: Error (auth/email-already-in-use).':'A user with that email already exists',
   'Firebase: Error (auth/user-not-found).': 'Invalid email or password',
   'Firebase: Error (auth/wrong-password).': 'Invalid email or password',
-  'Firebase: Password should be at least 6 characters (auth/weak-password).':
-    'Password should be at least 6 characters',
+  'Firebase: Password should be at least 6 characters (auth/weak-password).':'Password should be at least 6 characters',
   'Firebase: Error (auth/invalid-email).': 'Invalid email',
-  'Firebase: Error (auth/operation-not-allowed).':
-    'This operation is not allowed',
+  'Firebase: Error (auth/operation-not-allowed).':'This operation is not allowed',
   'Firebase: Error (auth/invalid-credential).': 'Invalid credential',
   'Firebase: Error (auth/user-disabled).': 'This user has been disabled',
-  'Firebase: Error (auth/invalid-verification-code).':
-    'Invalid verification code',
+  'Firebase: Error (auth/invalid-verification-code).':'Invalid verification code',
   'Firebase: Error (auth/invalid-verification-id).': 'Invalid verification id',
-  'Firebase: Error (auth/missing-verification-code).':
-    'Missing verification code',
+  'Firebase: Error (auth/missing-verification-code).':'Missing verification code',
   'Firebase: Error (auth/missing-verification-id).': 'Missing verification id',
-  'Firebase: Error (auth/credential-already-in-use).':
-    'This credential is already in use',
+  'Firebase: Error (auth/credential-already-in-use).':'This credential is already in use',
+  'Firebase: Error (auth/weak-password).': 'Password is too weak',
+  'Firebase: Error (auth/account-exists-with-different-credential).':'An account already exists with the same email address but different sign-in credentials',
+  'Firebase: Error (auth/network-request-failed).': 'Network error occurred',
+  'Firebase: Error (auth/too-many-requests).':'Too many requests. Please try again later',
+  'Firebase: Error (auth/app-deleted).': 'The Firebase app has been deleted',
+  'Firebase: Error (auth/app-not-authorized).':'The Firebase app is not authorized to use Firebase Authentication',
+  'Firebase: Error (auth/argument-error).': 'An invalid argument was provided',
+  'Firebase: Error (auth/internal-error).':'An internal error occurred while attempting to authenticate the user',
 };
