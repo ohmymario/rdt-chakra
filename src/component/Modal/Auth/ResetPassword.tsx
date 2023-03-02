d89b83d95db658aa5097f57222025fa8b22f3953
Mario Ballesteros <mariob360@gmail.com>
2023-03-01 18:06:18 -0800

    create most of resetpassword component

---
+++ src/component/Modal/Auth/ResetPassword.tsx
@@ -0,0 +1,106 @@
+import {
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  Input,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+
+import { BsDot, BsReddit } from 'react-icons/bs';
+
+import { FunctionComponent } from 'react';
+
+// change modal state
+import { authModalState } from '@/atoms/authModalAtom';
+import { useSetRecoilState } from 'recoil';
+
+interface ResetPasswordProps {}
+
+const ResetPassword: FunctionComponent<ResetPasswordProps> = (
+  props: ResetPasswordProps
+) => {
+  // setting between login and signup and reset password
+  const setAuthModalState = useSetRecoilState(authModalState);
+
+  const inputStyles = {
+    fontSize: '10pt',
+    _placeholder: {
+      color: 'gray.500',
+    },
+    _hover: {
+      bg: 'white',
+      border: '1px solid',
+      borderColor: 'blue.500',
+    },
+    _focus: {
+      outline: 'white',
+      bg: 'white',
+      border: '1px solid',
+      borderColor: 'blue.500',
+    },
+    bg: 'gray.50',
+  };
+
+  return (
+    <>
+      <VStack spacing={2} align='stretch' mb={2}>
+        <VStack spacing={3} textAlign='center'>
+          <Icon as={BsReddit} color='brand.100' fontSize={40} />
+          <Heading size='md'>Reset your password</Heading>
+          <Text fontSize='sm'>
+            Enter the email associated with your account and we&apos;ll send you
+            a reset link.
+          </Text>
+        </VStack>
+
+        <VStack spacing={4} textAlign='center' align='stretch'>
+          <Input type='email' placeholder='Email' {...inputStyles} />
+          <Button height='36px' variant='auth' type='submit'>
+            Log In
+          </Button>
+        </VStack>
+
+        <Flex
+          fontSize='9pt'
+          justify='center'
+          alignItems='center'
+          color='blue.500'
+        >
+          <Text
+            color='blue.500'
+            fontWeight={700}
+            cursor='pointer'
+            textDecoration={'underline'}
+            onClick={() =>
+              setAuthModalState({
+                isOpen: true,
+                view: 'login',
+              })
+            }
+          >
+            Login
+          </Text>
+          <Icon as={BsDot} />
+          <Text
+            color='blue.500'
+            fontWeight={700}
+            cursor='pointer'
+            textDecoration={'underline'}
+            onClick={() =>
+              setAuthModalState({
+                isOpen: true,
+                view: 'signup',
+              })
+            }
+          >
+            Sign Up
+          </Text>
+        </Flex>
+      </VStack>
+    </>
+  );
+};
+
+export default ResetPassword;
