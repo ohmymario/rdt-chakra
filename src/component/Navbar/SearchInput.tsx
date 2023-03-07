7beb62cf0e066110a75d197ccdefe1a741c52687
Mario Ballesteros <mariob360@gmail.com>
2023-03-07 12:18:19 -0800

    remove margin and update quotes to single quotes

--- src/component/Navbar/SearchInput.tsx
+++ src/component/Navbar/SearchInput.tsx
@@ -1,6 +1,6 @@
-import { SearchIcon } from "@chakra-ui/icons";
-import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
-import { FunctionComponent } from "react";
+import { SearchIcon } from '@chakra-ui/icons';
+import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { FunctionComponent } from 'react';
 
 interface SearchInputProps {
   // user: User;
@@ -10,30 +10,30 @@ const SearchInput: FunctionComponent<SearchInputProps> = (
   props: SearchInputProps
 ) => {
   return (
-    <Flex flexGrow={1} mr={2} alignItems="center">
+    <Flex flexGrow={1} alignItems='center'>
       <InputGroup>
         <InputLeftElement
-          pointerEvents="none"
-          height="100%"
-          children={<SearchIcon boxSize={"5"} color="gray.500" mt="1px" />}
+          pointerEvents='none'
+          height='100%'
+          children={<SearchIcon boxSize={'5'} color='gray.500' mt='1px' />}
         />
         <Input
-          placeholder="Search Reddit"
-          fontSize="10pt"
-          height="34px"
-          bg={"gray.50"}
+          placeholder='Search Reddit'
+          fontSize='10pt'
+          height='34px'
+          bg={'gray.50'}
           _placeholder={{
-            color: "gray.500",
+            color: 'gray.500',
           }}
           _hover={{
-            bg: "white",
-            border: "1px solid",
-            borderColor: "blue.500",
+            bg: 'white',
+            border: '1px solid',
+            borderColor: 'blue.500',
           }}
           _focus={{
-            outline: "none",
-            border: "1px solid",
-            borderColor: "blue.500",
+            outline: 'none',
+            border: '1px solid',
+            borderColor: 'blue.500',
           }}
         />
       </InputGroup>
