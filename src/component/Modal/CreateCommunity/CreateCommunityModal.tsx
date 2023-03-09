ca29d3d5ea8bbc9a1530c235f93340311bb2051e
Mario Ballesteros <mariob360@gmail.com>
2023-03-08 16:57:07 -0800

    progress on community modal

--- src/component/Modal/CreateCommunity/CreateCommunityModal.tsx
+++ src/component/Modal/CreateCommunity/CreateCommunityModal.tsx
@@ -7,6 +7,16 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
+  Box,
+  Heading,
+  Text,
+  Input,
+  Radio,
+  VStack,
+  Divider,
+  Checkbox,
+  Highlight,
+  HStack,
 } from '@chakra-ui/react';
 import { FunctionComponent } from 'react';
 
@@ -20,23 +30,102 @@ const CreateCommunityModal: FunctionComponent<CreateCommunityModalProps> = (
 ) => {
   const { open, handleClose } = props;
 
+  const handleCreateCommunity = () => {
+    console.log('Create Community');
+  };
+
   return (
     <>
       <Modal isOpen={open} onClose={() => handleClose()}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader fontSize='md' fontWeight='500'>
+            Create a community
+          </ModalHeader>
           <ModalCloseButton />
+
+          <Divider />
+
           <ModalBody>
-            MODAL BODY Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consectetur, quos.
+            <VStack spacing={6}>
+              <VStack width={'100%'} align='flex-start'>
+                <Box>
+                  <Heading as='h4' size='sm' mb={2} fontWeight='500'>
+                    Name
+                  </Heading>
+                  <Text>
+                    Community names including capitalization cannot be changed.
+                  </Text>
+                </Box>
+                <Input placeholder='r/' size='md' />
+                <Text>21 characters remaining</Text>
+              </VStack>
+
+              <VStack width={'100%'} align='flex-start'>
+                <Heading as='h4' size='sm' mb={2} fontWeight='500'>
+                  Community Type
+                </Heading>
+                <Radio size='md' name='1'>
+                  <HStack>
+                    <Text display='inline'>Public</Text>
+                    <Text fontSize='8pt' color='gray.500'>
+                      Anyone can view, post, and comment to this community
+                    </Text>
+                  </HStack>
+                </Radio>
+                <Radio size='md' name='1'>
+                  <HStack>
+                    <Text>Restricted</Text>
+                    <Text fontSize='8pt' color='gray.500'>
+                      Anyone can view this community, but only approved users
+                      can post
+                    </Text>
+                  </HStack>
+                </Radio>
+                <Radio size='md' name='1'>
+                  <Text>Private</Text>
+                  <Text fontSize='8pt' color='gray.500'>
+                    Only approved users can view and submit to this community{' '}
+                  </Text>
+                </Radio>
+              </VStack>
+
+              <VStack width={'100%'} align='flex-start'>
+                <Heading as='h4' size='sm' mb={2} fontWeight='500'>
+                  Adult Content
+                </Heading>
+                <Checkbox size={'lg'}>
+                  <Highlight
+                    query='NSFW'
+                    styles={{
+                      px: '1',
+                      py: '1',
+                      bg: 'red.400',
+                      color: 'white',
+                      fontSize: '2xs',
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    NSFW 18+ year old community
+                  </Highlight>
+                </Checkbox>
+              </VStack>
+            </VStack>
           </ModalBody>
 
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={() => handleClose()}>
-              Close
+          <ModalFooter bg='gray.100' borderRadius={'lg'}>
+            <Button
+              colorScheme='blue'
+              variant={'outline'}
+              height='32px'
+              mr={3}
+              onClick={() => handleClose()}
+            >
+              Cancel
             </Button>
-            <Button variant='ghost'>Create Community</Button>
+            <Button height='32px' onClick={() => handleCreateCommunity()}>
+              Create Community
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
