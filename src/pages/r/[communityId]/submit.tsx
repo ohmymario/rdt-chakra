cf95e706a44a7fbcfdc80f9bb11e8b2b59db9e4a
Mario Ballesteros <mariob360@gmail.com>
2023-03-14 18:00:35 -0700

    create community submit component

---
+++ src/pages/r/[communityId]/submit.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react';
+
+interface SubmitProps {}
+
+const Submit: FunctionComponent<SubmitProps> = (props: SubmitProps) => {
+  return <div>Submit</div>;
+};
+
+export default Submit;
