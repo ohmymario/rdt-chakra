38f9e3933d89892e077f6215d48bc505fdced165
Mario Ballesteros <mariob360@gmail.com>
2023-03-14 18:00:22 -0700

    create community component

---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'react';
+
+interface CommunityPageProps {}
+
+const CommunityPage: FunctionComponent<CommunityPageProps> = (
+  props: CommunityPageProps
+) => {
+  return <div>CommunityPage</div>;
+};
+
+export default CommunityPage;
