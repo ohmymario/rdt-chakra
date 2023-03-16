9b6bc6dd8672250c754f68449dc0df436d4dce8f
Mario Ballesteros <mariob360@gmail.com>
2023-03-16 13:32:00 -0700

    create base layout for pagecontent

---
+++ src/component/Layout/PageContent.tsx
@@ -0,0 +1,38 @@
+import { Flex, HStack } from '@chakra-ui/react';
+import { FunctionComponent } from 'react';
+
+// expects 2 children
+interface PageContentProps {
+  children: [React.ReactNode, React.ReactNode];
+}
+
+const PageContent: FunctionComponent<PageContentProps> = (props) => {
+  const { children } = props;
+  return (
+    <Flex justify='center'>
+      <Flex
+        justify='center'
+        maxW='976px'
+        width='95%'
+        align='center'
+        gap={'1.5rem'}
+      >
+        {/* LHS */}
+        <Flex flex='1'>{children[0]}</Flex>
+
+        {/* RHS */}
+        <Flex
+          display={{
+            base: 'none',
+            lg: 'flex',
+          }}
+          flex={{ base: '0 0 312px' }}
+        >
+          {children[1]}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default PageContent;
