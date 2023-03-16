7fee59d0d52360c9f38864eab2a4705a3df018ad
Mario Ballesteros <mariob360@gmail.com>
2023-03-16 16:14:18 -0700

    create community state and create global state

--- src/atoms/communitiesAtom.tsx
+++ src/atoms/communitiesAtom.tsx
@@ -3,6 +3,7 @@ import { atom } from 'recoil';
 
 type AccessLevel = 'public' | 'restricted' | 'private';
 
+// Community - communities collection
 export interface Community {
   createdAt: Timestamp;
   creatorId: string;
@@ -13,3 +14,19 @@ export interface Community {
   imageURL?: string;
   id: string;
 }
+
+// User Community State - users collection
+interface CommunityState {
+  mySnippets: {
+    communityId: string;
+    isModerator?: boolean;
+    imageURL?: string;
+  }[];
+}
+
+export const communitiesState = atom<CommunityState>({
+  key: 'communitiesState',
+  default: {
+    mySnippets: [],
+  },
+});
