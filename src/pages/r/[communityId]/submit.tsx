e7163657e96eb83d98303741c5b231f435c60d88
Mario Ballesteros <mariob360@gmail.com>
2023-03-23 15:11:52 -0700

    use the newpostform in the submit component

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,7 +1,21 @@
+import PageContent from '@/component/Layout/PageContent';
+import NewPostForm from '@/component/Posts/NewPostForm';
+import { Box, Divider, Text, VStack } from '@chakra-ui/react';
 import { FunctionComponent } from 'react';
 
 const SubmitPostPage: FunctionComponent = (props) => {
-  return <div>Submit</div>;
+  return (
+    <PageContent>
+      <VStack w='100%'>
+        <Box borderBottom='1px solid white' w='100%' p='14px 0' mb='8px'>
+          <Text>Create a post</Text>
+        </Box>
+        <NewPostForm />
+      </VStack>
+
+      <>About Community</>
+    </PageContent>
+  );
 };
 
 export default SubmitPostPage;
