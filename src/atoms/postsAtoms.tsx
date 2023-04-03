0694b6c3391be8b28f153e96bdccee0f7eb68946
Mario Ballesteros <mariob360@gmail.com>
2023-04-03 12:33:08 -0700

    feat: Add TypeScript types for Post data model

---
+++ src/atoms/postsAtoms.tsx
@@ -0,0 +1,30 @@
+import { Timestamp } from 'firebase/firestore';
+import { atom } from 'recoil';
+
+export interface Post {
+  id: string;
+  communityId: string;
+  creatorId: string;
+  title: string;
+  body: string;
+  numberOfComments: number;
+  voteStatus: number;
+  imageURL?: string;
+  communityImageURL?: string;
+  createdAt: Timestamp;
+}
+
+export interface PostState {
+  selectedPost: Post | null;
+  posts: Post[];
+}
+
+const defaultPostState: PostState = {
+  selectedPost: null,
+  posts: [],
+};
+
+export const postsState = atom<PostState>({
+  key: 'postsState',
+  default: defaultPostState,
+});
