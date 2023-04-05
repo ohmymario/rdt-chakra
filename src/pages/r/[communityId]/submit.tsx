3cc8e2a275ee1b29f0a520150376793fb91e3b65
Mario Ballesteros <mariob360@gmail.com>
2023-04-04 20:13:27 -0700

    feat: pass user into NewPostForm component

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,16 +1,19 @@
 import PageContent from '@/component/Layout/PageContent';
 import NewPostForm from '@/component/Posts/NewPostForm';
-import { Box, Divider, Text, VStack } from '@chakra-ui/react';
+import { auth } from '@/firebase/clientApp';
+import { Box, Text, VStack } from '@chakra-ui/react';
 import { FunctionComponent } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const SubmitPostPage: FunctionComponent = (props) => {
+  const [user] = useAuthState(auth);
   return (
     <PageContent>
       <VStack w='100%'>
         <Box borderBottom='1px solid white' w='100%' p='14px 0' mb='8px'>
           <Text>Create a post</Text>
         </Box>
-        <NewPostForm />
+        {user && <NewPostForm user={user} />}
       </VStack>
 
       <>About Community</>
