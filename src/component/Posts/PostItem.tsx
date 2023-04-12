5c01f26802576500b40e3be747848036f8ab430a
Mario Ballesteros <mariob360@gmail.com>
2023-04-11 21:26:20 -0700

    feat: PostItem now shows relative time

--- src/component/Posts/PostItem.tsx
+++ src/component/Posts/PostItem.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from 'react';
 import { Post } from '@/atoms/postsAtoms';
-import { Box, Flex, Grid, Heading, Icon, Image, Text } from '@chakra-ui/react';
+import { Box, Flex, Grid, Heading, HStack, Icon, Image, Stack, Text, VStack } from '@chakra-ui/react';
+import { Timestamp } from 'firebase/firestore';
 
 // Icons
 import { AiOutlineDelete } from 'react-icons/ai';
@@ -41,6 +42,33 @@ const PostItem: FunctionComponent<PostItemProps> = (props) => {
     creatorDisplayName,
   } = post;
 
+  // ChatGPT method to convert firebase timestamp to relative string
+  function timestampToRelativeString(timestamp: Timestamp) {
+    const now = new Date();
+    const timeDifference = now.getTime() - timestamp.toDate().getTime();
+
+    const secondsInMs = 1000;
+    const minutesInMs = secondsInMs * 60;
+    const hoursInMs = minutesInMs * 60;
+    const daysInMs = hoursInMs * 24;
+    const weeksInMs = daysInMs * 7;
+    const monthsInMs = daysInMs * 30.44; // Using the average number of days in a month (365.25 / 12)
+
+    if (timeDifference < daysInMs) {
+      const hours = Math.round(timeDifference / hoursInMs);
+      return `${hours} hour${hours === 1 ? '' : 's'} ago`;
+    } else if (timeDifference < weeksInMs) {
+      const days = Math.round(timeDifference / daysInMs);
+      return `${days} day${days === 1 ? '' : 's'} ago`;
+    } else if (timeDifference < monthsInMs) {
+      const weeks = Math.round(timeDifference / weeksInMs);
+      return `${weeks} week${weeks === 1 ? '' : 's'} ago`;
+    } else {
+      const months = Math.round(timeDifference / monthsInMs);
+      return `${months} month${months === 1 ? '' : 's'} ago`;
+    }
+  }
+
   return (
     <Flex
       border='1px solid'
@@ -54,7 +82,7 @@ const PostItem: FunctionComponent<PostItemProps> = (props) => {
       onClick={onSelectPost}
     >
       {/* VOTING COLUMN */}
-      <Flex flexDir='column' alignItems='center' justifyContent='flex-start' w={'40px'} p={2} bg='gray.300' mr='8px'>
+      <Flex flexDir='column' alignItems='center' justifyContent='flex-start' w={'40px'} p={2} bg='gray.300'>
         <Icon
           as={userVoteValue === 1 ? IoArrowUpCircleSharp : IoArrowUpCircleOutline}
           color={userVoteValue === 1 ? 'brand.100' : 'gray.400'}
@@ -73,17 +101,25 @@ const PostItem: FunctionComponent<PostItemProps> = (props) => {
       </Flex>
 
       {/* POST DATA */}
-      <Flex flexDir='column' justifyContent='center' flexGrow={1}>
-        <Flex fontSize={'xs'}>
-          <Text>Posted by {creatorDisplayName}</Text>
-          <Text>TIME</Text>
-        </Flex>
-        <Heading as={'h3'} fontSize='lg' fontWeight={'normal'}>
-          {title}
-        </Heading>
-        <Text noOfLines={[4]} maxH='376px' fontSize={'s'}>
-          {body}
-        </Text>
+      <Flex flexDir='column' flexGrow={1} m='8px'>
+        {/* TEXT */}
+        <VStack spacing={1} align='flex-start'>
+          <HStack spacing={1} fontSize={'xs'}>
+            {/* {communityImageURL && (
+            <Link href={`/communities/${communityId}`}>
+              <Image boxSize='20px' objectFit='cover' src={communityImageURL} alt={title} cursor='pointer' />
+            </Link>
+          )} */}
+            <Text>Posted by u/{creatorDisplayName}</Text>
+            <Text>{timestampToRelativeString(createdAt as Timestamp)}</Text>
+          </HStack>
+          <Heading as={'h3'} fontSize='lg' fontWeight={'normal'}>
+            {title}
+          </Heading>
+          <Text noOfLines={[4]} maxH='376px' fontSize={'s'}>
+            {body}
+          </Text>
+        </VStack>
 
         {/* POST IMAGE */}
         {imageURL && (
