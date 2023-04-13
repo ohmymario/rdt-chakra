d59abef51fa908fecffec11a7ab1aeb7f7c2b58e
Mario Ballesteros <mariob360@gmail.com>
2023-04-12 22:37:31 -0700

    feat: add styles to PostItem
    add styles to more closely resemble a community post

--- src/component/Posts/PostItem.tsx
+++ src/component/Posts/PostItem.tsx
@@ -28,7 +28,42 @@ interface PostItemProps {
   onSelectPost: () => void;
 }
 
+// ChatGPT method to convert firebase timestamp to relative string
+function timestampToRelativeString(timestamp: Timestamp) {
+  const now = new Date();
+  const timeDifference = now.getTime() - timestamp.toDate().getTime();
+
+  const secondsInMs = 1000;
+  const minutesInMs = secondsInMs * 60;
+  const hoursInMs = minutesInMs * 60;
+  const daysInMs = hoursInMs * 24;
+  const weeksInMs = daysInMs * 7;
+  const monthsInMs = daysInMs * 30.44; // Using the average number of days in a month (365.25 / 12)
+
+  if (timeDifference < hoursInMs) {
+    const minutes = Math.round(timeDifference / minutesInMs);
+    return `${minutes} minute${minutes === 1 ? '' : 's'} ago`;
+  } else if (timeDifference < daysInMs) {
+    const hours = Math.round(timeDifference / hoursInMs);
+    return `${hours} hour${hours === 1 ? '' : 's'} ago`;
+  } else if (timeDifference < weeksInMs) {
+    const days = Math.round(timeDifference / daysInMs);
+    return `${days} day${days === 1 ? '' : 's'} ago`;
+  } else if (timeDifference < monthsInMs) {
+    const weeks = Math.round(timeDifference / weeksInMs);
+    return `${weeks} week${weeks === 1 ? '' : 's'} ago`;
+  } else {
+    const months = Math.round(timeDifference / monthsInMs);
+    return `${months} month${months === 1 ? '' : 's'} ago`;
+  }
+}
+
 const PostItem: FunctionComponent<PostItemProps> = (props) => {
+  // TODO: FIND A WAY TO DO THIS WITHOUT USING useRouter SEVERAL TIMES
+  const router = useRouter();
+  const pathname = router.pathname;
+  console.log(pathname === '/r/[communityId]');
+
   const { post, userIsCreator, userVoteValue, onVote, onDeletePost, onSelectPost } = props;
   const {
     id,
@@ -44,40 +79,6 @@ const PostItem: FunctionComponent<PostItemProps> = (props) => {
     creatorDisplayName,
   } = post;
 
-  // ChatGPT method to convert firebase timestamp to relative string
-  function timestampToRelativeString(timestamp: Timestamp) {
-    const now = new Date();
-    const timeDifference = now.getTime() - timestamp.toDate().getTime();
-
-    const secondsInMs = 1000;
-    const minutesInMs = secondsInMs * 60;
-    const hoursInMs = minutesInMs * 60;
-    const daysInMs = hoursInMs * 24;
-    const weeksInMs = daysInMs * 7;
-    const monthsInMs = daysInMs * 30.44; // Using the average number of days in a month (365.25 / 12)
-
-  if (timeDifference < hoursInMs) {
-    const minutes = Math.round(timeDifference / minutesInMs);
-    return `${minutes} minute${minutes === 1 ? '' : 's'} ago`;
-  } else if (timeDifference < daysInMs) {
-      const hours = Math.round(timeDifference / hoursInMs);
-      return `${hours} hour${hours === 1 ? '' : 's'} ago`;
-    } else if (timeDifference < weeksInMs) {
-      const days = Math.round(timeDifference / daysInMs);
-      return `${days} day${days === 1 ? '' : 's'} ago`;
-    } else if (timeDifference < monthsInMs) {
-      const weeks = Math.round(timeDifference / weeksInMs);
-      return `${weeks} week${weeks === 1 ? '' : 's'} ago`;
-    } else {
-      const months = Math.round(timeDifference / monthsInMs);
-      return `${months} month${months === 1 ? '' : 's'} ago`;
-    }
-  }
-
-  // TODO: FIND A WAY TO DO THIS WITHOUT USING useRouter SEVERAL TIMES
-  const router = useRouter();
-  const pathname = router.pathname;
-  console.log(pathname === '/r/[communityId]');
   const containerStyles = {
     border: '1px solid',
     bg: 'white',
@@ -119,30 +120,30 @@ const PostItem: FunctionComponent<PostItemProps> = (props) => {
       </Flex>
 
       {/* POST DATA */}
-      <Flex flexDir='column' flexGrow={1} m='8px'>
+      <VStack align={'flex-start'} flexDir='column' flexGrow={1} m='8px 8px 2px 4px' spacing={2}>
         {/* TEXT */}
-        <VStack spacing={1} align='flex-start'>
-          <HStack spacing={1} fontSize={'xs'}>
+        <VStack align='flex-start' border={'1px solid red'} spacing={2} ml='4px'>
+          <HStack fontSize={'xs'} spacing={1}>
             {pathname != '/r/[communityId]' && (
               <Link href={`/r/${communityId}`}>
-              <Image boxSize='20px' objectFit='cover' src={communityImageURL} alt={title} cursor='pointer' />
-            </Link>
+                <Image boxSize='20px' objectFit='cover' src={communityImageURL} alt={title} cursor='pointer' />
+              </Link>
             )}
             <Text>Posted by u/{creatorDisplayName}</Text>
-            <Text>{timestampToRelativeString(createdAt as Timestamp)}</Text>
+            <Text>{timestampToRelativeString(createdAt)}</Text>
           </HStack>
-          <Heading as={'h3'} fontSize='lg' fontWeight={'normal'}>
+          <Heading fontSize='lg' as={'h3'} fontWeight={'600'}>
             {title}
           </Heading>
-          <Text noOfLines={[4]} maxH='376px' fontSize={'s'}>
+          <Text fontSize={'s'} noOfLines={[4]} maxH='376px'>
             {body}
           </Text>
         </VStack>
 
         {/* POST IMAGE */}
         {imageURL && (
-          <Grid justifyContent='center' alignItems='center' w={'100%'}>
-            <Image boxSize='512px' objectFit='cover' src={imageURL} alt={title} />
+          <Grid justifyContent='center' alignItems='center' w={'100%'} pl='4px'>
+            <Image boxSize='512px' objectFit='cover' src={imageURL} alt={`image of ${title}`} />
           </Grid>
         )}
 
@@ -171,7 +172,7 @@ const PostItem: FunctionComponent<PostItemProps> = (props) => {
                 <Text>Delete</Text>
               </>
             )}
-      </Flex>
+          </Flex>
         </HStack>
       </VStack>
     </Flex>
