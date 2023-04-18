5cc18a705c9feea6ff5cee8eb73802c6d7c17158
Mario Ballesteros <mariob360@gmail.com>
2023-04-18 14:52:11 -0700

    feat: add functionality to onDeletePost
    this method now deletes image and post date from the firestore and storage. The postStateValue is then updated to reflect this change.

--- src/hooks/usePostData.tsx
+++ src/hooks/usePostData.tsx
@@ -1,8 +1,7 @@
-import { useState, useEffect } from 'react';
-import { collection, query, where, orderBy, getDocs } from 'firebase/firestore';
-import { firestore } from '@/firebase/clientApp';
-import { Community } from '@/atoms/communitiesAtom';
-import { postsState } from '@/atoms/postsAtoms';
+import { Post, postsState } from '@/atoms/postsAtoms';
+import { firestore, storage } from '@/firebase/clientApp';
+import { deleteDoc, doc } from 'firebase/firestore';
+import { deleteObject, ref } from 'firebase/storage';
 import { useRecoilState } from 'recoil';
 
 const usePosts = () => {
@@ -18,9 +17,30 @@ const usePosts = () => {
     alert('Selecting a post not implemented yet');
   };
 
-  // WHEN USER DELETES A POST
-  const onDeletePost = async () => {
-    alert('Deleting a post not implemented yet');
+  // WHEN USER DELETES THEIR POST
+  const onDeletePost = async (post: Post): Promise<boolean> => {
+    try {
+      // Check for Image
+      if (post.imageURL) {
+        // Find the Image in Storage
+        const imageRef = ref(storage, `posts/${post.id}/image`);
+        // Delete the Image
+        await deleteObject(imageRef);
+      }
+      // Find The Post in Firestore
+      // Delete The Post
+      const postRef = doc(firestore, 'posts', post.id!);
+      await deleteDoc(postRef);
+      // Update Recoil Dtate
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: prev.posts.filter((oldPost) => oldPost.id !== post.id),
+      }));
+      return true;
+    } catch (error: any) {
+      console.log(error);
+      return false;
+    }
   };
 
   return {
