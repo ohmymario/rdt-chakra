8136ac181dee1c0c4851eaa126eec831cab73b8d
Mario Ballesteros <mariob360@gmail.com>
2023-04-27 11:30:11 -0700

    feat: create the header for the about component

--- src/component/Community/About.tsx
+++ src/component/Community/About.tsx
@@ -1,6 +1,8 @@
-import { Community } from '@/atoms/communitiesAtom';
-import { Box, VStack, Divider } from '@chakra-ui/react';
 import { FunctionComponent } from 'react';
+import { Community } from '@/atoms/communitiesAtom';
+import { Box, VStack, Divider, Center, Flex, Text, Icon, HStack, Stack } from '@chakra-ui/react';
+import { HiOutlineDotsHorizontal } from 'react-icons/hi';
+import { RiCakeLine } from 'react-icons/ri';
 
 interface AboutProps {
   communityData: Community;
@@ -11,12 +13,17 @@ const About: FunctionComponent<AboutProps> = (props) => {
   const { createdAt, creatorId, name, nsfw, numberOfMembers, type, imageURL, id } = communityData;
 
   return (
-    <VStack>
-      <Box>About Community ...</Box>
-      <Box>
-        <Box>230140234 memebrs</Box>
-        <Box>1231 online</Box>
-      </Box>
+    <Box alignSelf='flex-start' width='100%'>
+      {/* HEADER */}
+      <Flex justify='space-between' align='center' bg='blue.400' color='white' p={3} borderRadius='4px 4px 0 0'>
+        <Text>About Community</Text>
+        <Icon
+          as={HiOutlineDotsHorizontal}
+          _hover={{
+            cursor: 'pointer',
+          }}
+        />
+      </Flex>
       <Divider />
       <Box>
         <Box>DATE OF CREATION</Box>
