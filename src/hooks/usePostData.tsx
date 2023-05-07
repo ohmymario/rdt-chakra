278d882e92ec601043e83d08bc338af2379bdda5
Mario Ballesteros <mariob360@gmail.com>
2023-05-07 14:15:33 -0700

    feat: begin creation of the onVote method
    usePostData setup for the voting logic. Currently only creates a new vote. There is no contact with the firestore at the time.

--- src/hooks/usePostData.tsx
+++ src/hooks/usePostData.tsx
@@ -1,15 +1,67 @@
-import { Post, postsState } from '@/atoms/postsAtoms';
-import { firestore, storage } from '@/firebase/clientApp';
-import { deleteDoc, doc } from 'firebase/firestore';
+import { Post, postsState, PostVote } from '@/atoms/postsAtoms';
+import { auth, firestore, storage } from '@/firebase/clientApp';
+import { deleteDoc, doc, writeBatch } from 'firebase/firestore';
 import { deleteObject, ref } from 'firebase/storage';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRecoilState } from 'recoil';
 
 const usePosts = () => {
+  const [user] = useAuthState(auth);
   const [postStateValue, setPostStateValue] = useRecoilState(postsState);
 
   // VOTING
-  const onVote = async () => {
+  const onVote = async (post: Post, vote: number, communityId: string) => {
+    // 1. Whether or not its a down vote or up vote
+    // 2. Check if user has already voted
+    // 3. If user has already voted then check if its a down vote or up vote
     alert('Voting not implemented yet');
+
+    try {
+      const { voteStatus } = post;
+
+      // Check if the logged in user has a vote on the post community
+      const existingVote = postStateValue.postVotes.find((vote) => vote.postId === post.id);
+
+      // copy all needed data from the post state
+      const batch = writeBatch(firestore);
+      const updatedPost = { ...postStateValue.posts };
+      const updatedPosts = [...postStateValue.postVotes];
+      let voteChange = vote;
+
+      if (!existingVote) {
+        // location to store the votes
+        const postVoteRef = doc(firestore, 'users', `${user?.uid}/postVotes`);
+
+        // completely new vote object
+        const newVote: PostVote = {
+          id: postVoteRef.id,
+          postId: post.id!,
+          communityId: communityId,
+          voteValue: vote,
+        };
+
+        // prime this operation in the batch
+        batch.set(postVoteRef, newVote);
+
+        // if its a down vote or up vote
+        // total community votes = - 1
+        // total community votes = + 1;
+      } else {
+        if (removingVote) {
+          // removing vote
+          // add/substract 1 from post.voteStatus
+          // delete the postVote document from the user
+        } else {
+          // Changing their vote
+          // add/substract 2 from post.voteStatus
+          // update the postVote document from the user
+        }
+      }
+
+      // Update the post in the batch
+    } catch (error: any) {
+      console.log(error);
+    }
   };
 
   // WHEN USER CLICKS ON A POST
