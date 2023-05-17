595f1c3c807e18ff0d161eb0f7f2a53c5c47c93b
Mario Ballesteros <mariob360@gmail.com>
2023-05-17 12:18:34 -0700

    feat: Enhance PostPage with dynamic post fetching and community data
    - Add dynamic post fetching based on post ID from URL using Firebase Firestore.
    - Use `useCommunityData` hook to fetch and display community data.
    - Introduce `useRouter` from 'next/router' to access URL parameters.
    - Implement asynchronous fetchPost function that fetches individual post data.
    - Include useEffect to call fetchPost when a post is not selected or the URL's post ID changes.
    - Display 'About' component with community data if currentCommunity exists in communityStateValue.

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -1,22 +1,51 @@
+import { Post } from '@/atoms/postsAtoms';
+import About from '@/component/Community/About';
 import PageContent from '@/component/Layout/PageContent';
 import PostItem from '@/component/Posts/PostItem';
-import { auth } from '@/firebase/clientApp';
+import { auth, firestore } from '@/firebase/clientApp';
+import useCommunityData from '@/hooks/useCommunityData';
 import usePosts from '@/hooks/usePostData';
-import { FunctionComponent } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import { useRouter } from 'next/router';
+import { FunctionComponent, useCallback, useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 interface PostPageProps {}
 
 const PostPage: FunctionComponent<PostPageProps> = (props) => {
+  const router = useRouter();
   const [user] = useAuthState(auth);
   const { postStateValue, setPostStateValue, onDeletePost, onVote } = usePosts();
   const postMethods = { setPostStateValue, onDeletePost, onVote };
+  const { communityStateValue } = useCommunityData();
+
+  const { selectedPost } = postStateValue;
+  const { pid } = router.query;
+  const fetchPost = useCallback(
+    async (postId: string) => {
+      try {
+        const postDocRef = doc(firestore, 'posts', postId);
+        const postSnap = await getDoc(postDocRef);
+        setPostStateValue((prevState) => ({
+          ...prevState,
+          selectedPost: { ...postSnap.data(), id: postSnap.id } as Post,
+        }));
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [setPostStateValue]
+  );
+
+  useEffect(() => {
+    if (!selectedPost && pid) {
+      fetchPost(pid as string);
+    }
+  }, [selectedPost, pid, fetchPost]);
 
   return (
     <PageContent>
       <>
-        {/* Selected Post */}
-
         {postStateValue.selectedPost && (
           <PostItem
             post={postStateValue.selectedPost}
@@ -30,7 +59,7 @@ const PostPage: FunctionComponent<PostPageProps> = (props) => {
         {/* Comments */}
       </>
 
-      <>{/* About */}</>
+      <>{communityStateValue.currentCommunity && <About communityData={communityStateValue.currentCommunity} />}</>
     </PageContent>
   );
 };
