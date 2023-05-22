6dbd4bb4fe7acc6315eab0a2c9031a3c447b8294
Mario Ballesteros <mariob360@gmail.com>
2023-05-22 15:04:00 -0700

    feat: Refactor CommentInput for readability and maintainability
    - Refactor `userName` to `userDisplayName` for clarity.
    - Extract `handleCommentSubmit` function for encapsulation of comment submission behavior.
    - Abstract `commentInputStyles`, `commentButtonContainerStyles`, and `promptLoginStyles` as separate constants for cleanliness and potential reuse.
    - Use spread operator ({...styles}) to apply styles to components, improving readability.
    - Replace `disabled` prop with `isDisabled` in the Comment button to follow Chakra UI conventions.

--- src/component/Posts/Comments/CommentInput.tsx
+++ src/component/Posts/Comments/CommentInput.tsx
@@ -1,5 +1,5 @@
 import AuthButtons from '@/component/Navbar/RightContent/AuthButtons';
-import { Flex, Textarea, Button, Text } from '@chakra-ui/react';
+import { Button, Flex, FlexProps, Text, Textarea } from '@chakra-ui/react';
 import { User } from 'firebase/auth';
 
 interface CommentInputProps {
@@ -13,53 +13,64 @@ interface CommentInputProps {
 const CommentInput = (props: CommentInputProps) => {
   const { commentText, setCommentText, user, createLoading, onCreateComment } = props;
 
-  const userName = user?.email?.split('@')[0];
+  const userDisplayName = user?.email?.split('@')[0];
   const hasCommentText = Boolean(commentText.length);
+
+  const handleCommentSubmit = () => {
+    console.log('create comment');
+    onCreateComment(commentText);
+  };
+
+  const commentInputStyles = {
+    fontSize: '10pt',
+    borderRadius: '4px',
+    minHeight: '160px',
+    pb: 10,
+    _placeholder: { color: 'gray.500' },
+    _focus: { outline: 'none', bg: 'white', border: '1px solid black' },
+  };
+
+  const commentButtonContainerStyles: FlexProps = {
+    position: 'absolute',
+    left: '1px',
+    right: 0.1,
+    bottom: '1px',
+    justify: 'flex-end',
+    bg: 'gray.100',
+    p: '6px 8px',
+    borderRadius: '0px 0px 4px 4px',
+  };
+
+  const promptLoginStyles: FlexProps = {
+    align: 'center',
+    justify: 'space-between',
+    borderRadius: 2,
+    border: '1px solid',
+    borderColor: 'gray.100',
+    p: 4,
+  };
 
   return (
     <Flex direction='column' position='relative'>
       {user ? (
         <>
           <Text mb={1}>
-            Comment as <span style={{ color: '#3182CE' }}>{userName}</span>
+            Comment as <span style={{ color: '#3182CE' }}>{userDisplayName}</span>
           </Text>
           <Textarea
+            {...commentInputStyles}
             value={commentText}
             onChange={(event) => setCommentText(event.target.value)}
             placeholder='What are your thoughts?'
-            fontSize='10pt'
-            borderRadius={4}
-            minHeight='160px'
-            pb={10}
-            _placeholder={{ color: 'gray.500' }}
-            _focus={{
-              outline: 'none',
-              bg: 'white',
-              border: '1px solid black',
-            }}
           />
-          <Flex
-            position='absolute'
-            left='1px'
-            right={0.1}
-            bottom='1px'
-            justify='flex-end'
-            bg='gray.100'
-            p='6px 8px'
-            borderRadius='0px 0px 4px 4px'
-          >
-            <Button
-              height='26px'
-              disabled={!commentText.length}
-              isLoading={createLoading}
-              onClick={() => onCreateComment(commentText)}
-            >
+          <Flex {...commentButtonContainerStyles}>
+            <Button height='26px' isDisabled={!hasCommentText} isLoading={createLoading} onClick={handleCommentSubmit}>
               Comment
             </Button>
           </Flex>
         </>
       ) : (
-        <Flex align='center' justify='space-between' borderRadius={2} border='1px solid' borderColor='gray.100' p={4}>
+        <Flex {...promptLoginStyles}>
           <Text fontWeight={600}>Log in or sign up to leave a comment</Text>
           <AuthButtons />
         </Flex>
