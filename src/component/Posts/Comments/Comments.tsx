2bc1b02d7ab5ab59e2b822f460513846ed466bd4
Mario Ballesteros <mariob360@gmail.com>
2023-05-22 15:08:51 -0700

    feat: add CommentInput into Comments Component
    - create all necessary state values along with their types
    - Abstract FlexWrapperStyles as seperate variables for clean code
    - use spread operator to apply styles to Flex
    - pass all CommentInput props

--- src/component/Posts/Comments/Comments.tsx
+++ src/component/Posts/Comments/Comments.tsx
@@ -1,6 +1,8 @@
 import { Post } from '@/atoms/postsAtoms';
+import { Box, Flex, FlexProps } from '@chakra-ui/react';
 import { User } from 'firebase/auth';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import CommentInput from './CommentInput';
 
 interface CommentsProps {
   user: User;
@@ -10,6 +12,11 @@ interface CommentsProps {
 
 const Comments = (props: CommentsProps) => {
   const { user, selectedPost, communityId } = props;
+
+  const [commentText, setCommentText] = useState<string>('');
+  const [comments, setComments] = useState([]);
+  const [fetchLoading, setFetchLoading] = useState<boolean>(false);
+  const [createLoading, setCreateLoading] = useState<boolean>(false);
 
   const onCreateComment = async () => {
     console.log('create comment');
@@ -27,7 +34,28 @@ const Comments = (props: CommentsProps) => {
     getPostComments();
   }, []);
 
-  return <div>Commentsss</div>;
+  const FlexWrapperStyles: FlexProps = {
+    direction: 'column',
+    pl: 10,
+    pr: 4,
+    mb: 6,
+    fontSize: '10pt',
+    width: '100%',
+  };
+
+  return (
+    <Box bg='white' borderRadius='0px 0px 4px 4px' p={2}>
+      <Flex {...FlexWrapperStyles}>
+        <CommentInput
+          commentText={commentText}
+          setCommentText={setCommentText}
+          user={user}
+          createLoading={createLoading}
+          onCreateComment={onCreateComment}
+        />
+      </Flex>
+    </Box>
+  );
 };
 
 export default Comments;
