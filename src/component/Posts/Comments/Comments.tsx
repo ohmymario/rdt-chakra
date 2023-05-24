ffe25883ac44c26f6f6100d61db8439f749a579e
Mario Ballesteros <mariob360@gmail.com>
2023-05-23 22:13:39 -0700

    feat: Implement comment creation in Comments component
    - Include useSetRecoilState from recoil to manage the posts state.
    - Develop onCreateComment function to handle comment creation process which includes creating a new comment document in firestore, updating the post's numberOfComments field, and updating the global state to reflect these changes.
    - Modify CommentInput props within the return statement to include the newly created onCreateComment function.

--- src/component/Posts/Comments/Comments.tsx
+++ src/component/Posts/Comments/Comments.tsx
@@ -1,7 +1,11 @@
-import { Post } from '@/atoms/postsAtoms';
+import { Post, postsState } from '@/atoms/postsAtoms';
+import { firestore } from '@/firebase/clientApp';
 import { Box, Flex, FlexProps } from '@chakra-ui/react';
 import { User } from 'firebase/auth';
+import { collection, doc, increment, serverTimestamp, Timestamp, writeBatch } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { Comment } from './CommentItem';
 import CommentInput from './CommentInput';
 
 interface CommentsProps {
@@ -14,16 +18,64 @@ const Comments = (props: CommentsProps) => {
   const { user, selectedPost, communityId } = props;
 
   const [commentText, setCommentText] = useState<string>('');
-  const [comments, setComments] = useState([]);
+  const [comments, setComments] = useState<Comment[]>([]);
   const [fetchLoading, setFetchLoading] = useState<boolean>(false);
   const [createLoading, setCreateLoading] = useState<boolean>(false);
 
-  const onCreateComment = async (commentText: string) => {
-    console.log(`🚀 ~ onCreateComment ~ Creating a Comment:`, onCreateComment);
+  const setPostState = useSetRecoilState(postsState);
 
-    // create a comment document in firestore
-    // update post numberOfComments field by +1
-    // update global state to reflect changes
+  const onCreateComment = async () => {
+    console.log(`🚀 ~ onCreateComment ~ Creating a Comment:`, onCreateComment);
+    setCreateLoading(true);
+
+    try {
+      // create a comment document in firestore
+      const batch = writeBatch(firestore);
+      const firestoreCollection = collection(firestore, 'comments');
+      const commentDocRef = doc(firestoreCollection);
+
+      // All data needed for newComment
+      const { id } = commentDocRef;
+      const { uid, email } = user;
+      const { id: postId, title: postTitle } = selectedPost;
+      const creatorDisplayText = email!.split('@')[0];
+
+      const newComment: Comment = {
+        id,
+        creatorId: uid,
+        creatorDisplayText,
+        communityId: communityId,
+        postId: postId!,
+        postTitle,
+        text: commentText,
+        createdAt: serverTimestamp() as Timestamp,
+      };
+
+      batch.set(commentDocRef, newComment);
+
+      // update post numberOfComments field by +1
+      const postDocRef = doc(firestore, 'posts', postId!);
+      batch.update(postDocRef, {
+        numberOfComments: increment(1),
+      });
+
+      await batch.commit();
+
+      setCommentText('');
+      setComments((prevComments) => [...prevComments, newComment]);
+      setPostState((prev) => ({
+        ...prev,
+        selectedPost: {
+          ...prev.selectedPost,
+          numberOfComments: prev.selectedPost?.numberOfComments! + 1,
+        } as Post,
+      }));
+
+      // update global state to reflect changes
+    } catch (error) {
+      console.log('Error creating comment: ', error);
+    }
+    setCreateLoading(false);
   };
 
   const onDeleteComment = async (comment: any) => {
