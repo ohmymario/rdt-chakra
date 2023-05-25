d03738b8085a7ff1c671249df6770bfd6958825f
Mario Ballesteros <mariob360@gmail.com>
2023-05-25 15:17:00 -0700

    feat: create CommentsSkeleton component
    - Mimics the look of a comment
    - Accepts a number of comments for flexibility

---
+++ src/component/Posts/Comments/CommentsSkeleton.tsx
@@ -0,0 +1,23 @@
+import { Box, SkeletonCircle, SkeletonText } from '@chakra-ui/react';
+
+interface CommentsSkeletonProps {
+  numberOfComments: number;
+}
+
+const CommentsSkeleton = (props: CommentsSkeletonProps) => {
+  const { numberOfComments } = props;
+
+  const skeletonArray = [...Array(numberOfComments)];
+  return (
+    <>
+      {skeletonArray.map((_, index) => (
+        <Box key='index'>
+          <SkeletonCircle size='10' />
+          <SkeletonText mt='4' />
+        </Box>
+      ))}
+    </>
+  );
+};
+
+export default CommentsSkeleton;
