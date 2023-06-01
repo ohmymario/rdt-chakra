9bb08beb73eb374c06c865688b8731de166e3a01
Mario Ballesteros <mariob360@gmail.com>
2023-06-01 13:01:01 -0700

    feat: create directoryMenuAtoms
    - create atom state for the menu items
    - default state for each item
    - contains state to open the menu per item

---
+++ src/atoms/directoryMenuAtom.tsx
@@ -0,0 +1,36 @@
+import { IconType } from 'react-icons/lib';
+import { TiHome } from 'react-icons/ti';
+import { atom } from 'recoil';
+
+// TYPES
+interface DirectoryMenuItem {
+  displayText: string;
+  link: string;
+  icon: IconType;
+  iconColor: string;
+  ImageURL?: string;
+}
+
+interface DirectoryMenuState {
+  isOpen: boolean;
+  selectedMenuItem: DirectoryMenuItem;
+}
+
+// DEFAULTS
+export const defaultMenuItem: DirectoryMenuItem = {
+  displayText: 'Home',
+  link: '/',
+  icon: TiHome,
+  iconColor: 'black',
+};
+
+export const defaultMenuState: DirectoryMenuState = {
+  isOpen: false,
+  selectedMenuItem: defaultMenuItem,
+};
+
+// ATOM STATE
+export const directoryMenuState = atom<DirectoryMenuState>({
+  key: 'directoryMenuState',
+  default: defaultMenuState,
+});
