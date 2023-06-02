e90daac39a235a741e0e53b57c08ff721c270291
Mario Ballesteros <mariob360@gmail.com>
2023-06-02 14:36:49 -0700

    feat: update menu on refresh
    - useRecoilValue currentCommunity to recover community from the db
    - populate directoryMenuState from the currentCommunity

--- src/hooks/useDirectory.tsx
+++ src/hooks/useDirectory.tsx
@@ -1,10 +1,16 @@
+import { communitiesState } from '@/atoms/communitiesAtom';
 import { useRouter } from 'next/router';
-import { useRecoilState } from 'recoil';
+import { useEffect } from 'react';
+import { FaReddit } from 'react-icons/fa';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { DirectoryMenuItem, directoryMenuState } from '../atoms/directoryMenuAtom';
 
 const UseDirectory = () => {
   const router = useRouter();
   const [directoryState, setDirectoryState] = useRecoilState(directoryMenuState);
+
+  const communityStateValue = useRecoilValue(communitiesState);
+  const { currentCommunity } = communityStateValue;
 
   const onSelectMenuItem = (menuItem: DirectoryMenuItem) => {
     setDirectoryState((prev) => ({ ...prev, selectedMenuItem: menuItem }));
@@ -18,6 +24,23 @@ const UseDirectory = () => {
   const toggleMenuOpen = () => {
     setDirectoryState((prev) => ({ ...prev, isOpen: !prev.isOpen }));
   };
+
+  useEffect(() => {
+    if (currentCommunity) {
+      const packagedMenuItem = {
+        displayText: `r/${currentCommunity.id}`,
+        link: `/r/${currentCommunity.id}`,
+        icon: FaReddit,
+        iconColor: 'blue.500',
+        ImageURL: currentCommunity.imageURL,
+      };
+
+      setDirectoryState((prev) => ({
+        ...prev,
+        selectedMenuItem: packagedMenuItem,
+      }));
+    }
+  }, [currentCommunity]);
 
   return { directoryState, toggleMenuOpen, onSelectMenuItem };
 };
