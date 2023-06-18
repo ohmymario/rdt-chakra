372d257e59fb15871354f0c97e5d9ad980cfbba2
Mario Ballesteros <mariob360@gmail.com>
2023-06-18 15:28:20 -0700

    feat: personalHome component

---
+++ src/component/Community/PersonalHome.tsx
@@ -0,0 +1,51 @@
+import { Flex, Icon, Stack, Button, Text } from '@chakra-ui/react';
+import { FaReddit } from 'react-icons/fa';
+
+interface PersonalHomeProps {}
+
+const PersonalHome = (props: PersonalHomeProps) => {
+  return (
+    <Flex
+      direction='column'
+      bg='white'
+      borderRadius={4}
+      cursor='pointer'
+      border='1px solid'
+      borderColor='gray.300'
+      position='sticky'
+    >
+      <Flex
+        align='flex-end'
+        color='white'
+        p='6px 10px'
+        bg='blue.500'
+        height='34px'
+        borderRadius='4px 4px 0px 0px'
+        fontWeight={600}
+        bgImage='url(/images/redditPersonalHome.png)'
+        backgroundSize='cover'
+      ></Flex>
+      <Flex direction='column' p='12px'>
+        <Flex align='center' mb={2}>
+          <Icon as={FaReddit} fontSize={50} color='brand.100' mr={2} />
+          <Text fontWeight={600}>Home</Text>
+        </Flex>
+        <Stack spacing={3}>
+          <Text fontSize='9pt'>Your personal Reddit frontpage, built for you.</Text>
+
+          {/* TODO: Check if the user is signed in if not open up the login */}
+          {/* TODO: If they are signed in then scroll up and open menu */}
+          <Button height='30px'>Create Post</Button>
+
+          {/* TODO: Check if the user is signed in if not open up the login */}
+          {/* TODO: signed in then open up create community modal */}
+          <Button variant='outline' height='30px'>
+            Create Community
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default PersonalHome;
