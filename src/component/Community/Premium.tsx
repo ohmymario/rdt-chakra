3591c822a68d5b5eca6cdd9ecfa7e1a58c0b9764
Mario Ballesteros <mariob360@gmail.com>
2023-06-20 20:12:31 -0700

    refactor: move Premium styles into their own variable
    -seperate some styles from jsx

--- src/component/Community/Premium.tsx
+++ src/component/Community/Premium.tsx
@@ -1,19 +1,21 @@
-import { Button, Flex, Icon, Stack, Text } from '@chakra-ui/react';
+import { Button, Flex, FlexProps, Icon, Stack, Text } from '@chakra-ui/react';
 import { GiCheckedShield } from 'react-icons/gi';
 
 interface PremiumProps {}
 
+const flexWrapperStyles: FlexProps = {
+  direction: 'column',
+  bg: 'white',
+  borderRadius: 4,
+  cursor: 'pointer',
+  p: '12px',
+  border: '1px solid',
+  borderColor: 'gray.300',
+};
+
 const Premium = (props: PremiumProps) => {
   return (
-    <Flex
-      direction='column'
-      bg='white'
-      borderRadius={4}
-      cursor='pointer'
-      p='12px'
-      border='1px solid'
-      borderColor='gray.300'
-    >
+    <Flex {...flexWrapperStyles}>
       <Flex mb={2}>
         <Icon as={GiCheckedShield} fontSize={26} color='brand.100' mt={2} />
         <Stack spacing={1} fontSize='9pt' pl={2}>
