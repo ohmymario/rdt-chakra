2ced5df52299f04b21a19843511fa2162227dbf7
Mario Ballesteros <mariob360@gmail.com>
2023-06-21 14:32:33 -0700

    feat: create constants for NotFound Component
    - moved text into constants.js

---
+++ src/component/Community/NotFound/constants.tsx
@@ -0,0 +1,4 @@
+export const NOT_FOUND_TEXT_1 = "Sorry, there aren't any communities on Reddit with that name.";
+export const NOT_FOUND_TEXT_2 = 'This community may have been banned or the community name is incorrect.';
+export const FOOTER_TEXT =
+  'Use of this site constitutes acceptance of our User Agreement and Privacy Policy. ©2023 reddit inc. All rights reserved. REDDIT and the ALIEN Logo are registered trademarks of reddit inc.';
