499f7a97112387a997b7ce709afdf89a89440c3f
Mario Ballesteros <mariob360@gmail.com>
2023-06-21 21:20:57 -0700

    Refactor About component for readability and maintainability
    - Extract timestampToDate function to a separate utility file for reusability and improved readability.
    - Create separate AboutHeader and AboutWelcome components to simplify the About component.
    - Implement useImageUpload custom hook to encapsulate image upload logic.
    - Add isMounted state and useEffect hook to prevent hydration error with server-side rendering.
    - Use useAuthState hook from react-firebase-hooks to get the currently logged in user.
    - Use next/link for navigation to enable client-side navigation and improve performance.

--- src/component/Community/About/About.tsx
+++ src/component/Community/About/About.tsx
@@ -1,82 +1,28 @@
-import { communitiesState, Community } from '@/atoms/communitiesAtom';
-import { auth, firestore, storage } from '@/firebase/clientApp';
-import useSelecfile from '@/hooks/useSelectFile';
+import { Community } from '@/atoms/communitiesAtom';
+import { auth } from '@/firebase/clientApp';
+import { useImageUpload } from '@/hooks/useImageUpload';
 import { Box, Button, Divider, Flex, Icon, Image, Spinner, Stack, Text } from '@chakra-ui/react';
-import { doc, Timestamp, updateDoc } from 'firebase/firestore';
-import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 import Link from 'next/link';
-import { FunctionComponent, useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { FaReddit } from 'react-icons/fa';
-import { HiOutlineDotsHorizontal } from 'react-icons/hi';
-import { RiCakeLine } from 'react-icons/ri';
-import { useRecoilState } from 'recoil';
+import AboutHeader from './AboutHeader';
+import AboutWelcome from './AboutWelcome';
 
 interface AboutProps {
   communityData: Community;
 }
 
-function timestampToDate(timestampData: { seconds: number; nanoseconds: number }): string {
-  const timestamp = new Timestamp(timestampData.seconds, timestampData.nanoseconds);
-  const date = timestamp.toDate();
-  const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-  const month = monthNames[date.getMonth()];
-  const day = date.getDate();
-  const year = date.getFullYear();
-
-  return `${month}, ${day} ${year}`;
-}
-
-const About: FunctionComponent<AboutProps> = (props) => {
+const About = (props: AboutProps) => {
   // Grab the currently logged in user
   const [user] = useAuthState(auth);
 
   // Props
   const { communityData } = props;
   const { createdAt, creatorId, name, nsfw, numberOfMembers, type, imageURL, id } = communityData;
-
+  const { selectedFileRef, selectedFile, onSelectFile, uploadingImage, onUpdateImage } = useImageUpload(communityData);
   // Fix for hydration error
   const [isMounted, setIsMounted] = useState(false);
-
-  // Image Upload / Input Ref
-  const selectedFileRef = useRef<HTMLInputElement>(null);
-  const { selectedFile, onSelectFile, setSelectedFile } = useSelecfile();
-  const [uploadingImage, setUploadingImage] = useState(false);
-
-  const [communityStateValue, setCommunityStateValue] = useRecoilState(communitiesState);
-
-  const onUpdateImage = async () => {
-    if (!selectedFile) return;
-    setUploadingImage(true);
-
-    try {
-      // Uploading an image to a community, generating a download URL,
-      // and updating the community document with the image URL in Firestore.
-      const imageLocation = `communities/${id}/image`;
-      const imageRef = ref(storage, imageLocation);
-      await uploadString(imageRef, selectedFile, 'data_url');
-      const downloadURL = await getDownloadURL(imageRef);
-      await updateDoc(doc(firestore, 'communities', id), {
-        imageURL: downloadURL,
-      });
-
-      // Update the global state
-      setCommunityStateValue((prev) => ({
-        ...prev,
-        currentCommunity: {
-          ...communityData,
-          imageURL: downloadURL,
-        },
-      }));
-
-      // Operation Complete
-      setUploadingImage(false);
-    } catch (error) {
-      setUploadingImage(false);
-      console.error(error);
-    }
-  };
 
   // When the component is mounted, it sets the `isMounted` state to `true` using the `useEffect` hook.
   // This ensures that the `Link` component is only rendered on the client side, after the component is mounted,
@@ -93,30 +39,12 @@ const About: FunctionComponent<AboutProps> = (props) => {
 
   return (
     <Box alignSelf='flex-start' width='100%'>
-      {/* HEADER */}
-      <Flex justify='space-between' align='center' bg='blue.400' color='white' p={3} borderRadius='4px 4px 0 0'>
-        <Text fontWeight={700} fontSize='10pt'>
-          About Community
-        </Text>
-        <Icon
-          as={HiOutlineDotsHorizontal}
-          _hover={{
-            cursor: 'pointer',
-          }}
-        />
-      </Flex>
+      <AboutHeader />
 
       {/* BODY */}
       <Flex direction='column' p={3} bg='white' borderRadius='0 0 4px 4px'>
         <Stack>
-          <Text>
-            Welcome to r/{creatorId}, the place for almost anything {creatorId} related.
-          </Text>
-          <Flex width='100%' fontSize='10pt' gap={2} fontWeight='500' p={1}>
-            <Icon color={'gray.600'} as={RiCakeLine} fontSize={18} />
-            <Text color={'gray.400'}>Created {timestampToDate(createdAt)}</Text>
-          </Flex>
-
+          <AboutWelcome creatorId={creatorId} createdAt={createdAt} />
           <Divider />
 
           <Flex width='100%' p={2} fontSize='10pt' fontWeight={700}>
