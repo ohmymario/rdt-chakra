fc6d790c03acae05d857082bd548653681f34c8e
Mario Ballesteros <mariob360@gmail.com>
2023-06-30 15:12:29 -0700

    fix: remove old form from ResetPassword

--- src/component/Modal/Auth/ResetPassword/ResetPassword.tsx
+++ src/component/Modal/Auth/ResetPassword/ResetPassword.tsx
@@ -63,21 +63,6 @@ const ResetPassword: FunctionComponent<ResetPasswordProps> = (props: ResetPasswo
               Enter the email associated with your account and we&apos;ll send you a reset link.
             </Text>
 
-            <form onSubmit={(e) => handlePasswordReset(e)}>
-              {error && (
-                <Alert status='error' borderRadius='xl' mb={3}>
-                  <AlertIcon />
-                  {FIREBASE_ERRORS[error.message as keyof typeof FIREBASE_ERRORS]}
-                </Alert>
-              )}
-
-              <Input type='email' placeholder='Email' required onChange={(e) => handleEmail(e)} {...inputStyles} />
-
-              <Button height='36px' variant='auth' type='submit' width={'100%'} isLoading={sending}>
-                Reset Password
-              </Button>
-            </form>
-
             <ResetPasswordForm
               error={error}
               sending={sending}
