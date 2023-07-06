c28f9cfa55920cb80b3eac1ac6d4a216000b0681
Mario Ballesteros <mariob360@gmail.com>
2023-07-06 13:22:39 -0700

    refactor: abstract moderation menu into separate component

---
+++ src/component/Navbar/Directory/Communities/CommunitiesModerating.tsx
@@ -0,0 +1,40 @@
+import { CommunitySnippet } from '@/atoms/communitiesAtom';
+import { Box, Text } from '@chakra-ui/react';
+import { FaReddit } from 'react-icons/fa';
+import MenuListItem from './MenuListItem';
+
+interface CommunitiesModeratingProps {
+  mySnippets: CommunitySnippet[];
+}
+
+const communityHeaderStyles = {
+  paddingLeft: 3,
+  marginBottom: 1,
+  fontSize: '7pt',
+  fontWeight: 500,
+  color: 'gray.500',
+};
+
+const CommunitiesModerating = (props: CommunitiesModeratingProps) => {
+  const { mySnippets } = props;
+  const modCommunities = mySnippets.filter((community) => community.isModerator);
+
+  return (
+    <Box mt={3} mb={4}>
+      <Text {...communityHeaderStyles}>Moderating</Text>
+
+      {modCommunities.map((snippet) => (
+        <MenuListItem
+          key={snippet.communityId}
+          displayText={`r/${snippet.communityId}`}
+          link={`/r/${snippet.communityId}`}
+          icon={FaReddit}
+          iconColor='brand.100'
+          ImageURL={snippet.imageURL}
+        />
+      ))}
+    </Box>
+  );
+};
+
+export default CommunitiesModerating;
