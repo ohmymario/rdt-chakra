5e91998a9f8bbb4af809502acef1c51e1bb35239
Mario Ballesteros <mariob360@gmail.com>
2023-07-06 22:52:20 -0700

    refactor: improve CommunitiesListItem code readability and logic
    - Added constants for default icon and iconColor for clarity
    - Introduced DirectoryMenuItem for better type safety
    - Simplified onClick function by creating handleMenuItemClick
    - Updated conditional rendering for Image and Icon components

--- src/component/Navbar/Directory/Communities/CommunitiesListItem.tsx
+++ src/component/Navbar/Directory/Communities/CommunitiesListItem.tsx
@@ -2,6 +2,7 @@ import UseDirectory from '@/hooks/useDirectory';
 import { Flex, Icon, Image, MenuItem } from '@chakra-ui/react';
 import { IconType } from 'react-icons/lib';
 import { FaReddit } from 'react-icons/fa';
+import { DirectoryMenuItem } from '@/atoms/directoryMenuAtom';
 
 interface CommunitiesListItemProps {
   communityId: string;
@@ -10,34 +11,41 @@ interface CommunitiesListItemProps {
   ImageURL?: string;
 }
 
+const IconColorDefault = 'blue.500';
+const IconDefault = FaReddit;
+
 const CommunitiesListItem = (props: CommunitiesListItemProps) => {
-  const { communityId, icon = FaReddit, iconColor = 'blue.500', ImageURL } = props;
+  const { communityId, icon = IconDefault, iconColor = IconColorDefault, ImageURL } = props;
   const { onSelectMenuItem } = UseDirectory();
 
   const displayText = `r/${communityId}`;
   const link = `/r/${communityId}`;
 
+  const handleMenuItemClick = () => {
+    const MenuItemData: DirectoryMenuItem = {
+      displayText,
+      link,
+      icon,
+      iconColor,
+      ImageURL,
+    };
+
+    onSelectMenuItem(MenuItemData);
+  };
+
   return (
-    <MenuItem
-      width='100%'
-      fontSize='10pt'
-      _hover={{ bg: 'gray.100' }}
-      onClick={() => {
-        onSelectMenuItem({
-          displayText,
-          link,
-          icon,
-          iconColor,
-          ImageURL,
-        });
-      }}
-    >
+    <MenuItem width='100%' fontSize='10pt' _hover={{ bg: 'gray.100' }} onClick={handleMenuItemClick}>
       <Flex align='center'>
-        {ImageURL ? (
-          <Image src={ImageURL} borderRadius='full' boxSize='18px' mr={2} alt={`${displayText} icon`} />
-        ) : (
-          <Icon as={icon} fontSize={20} mr={2} color={iconColor} />
+        {ImageURL && (
+          <Image
+            src={ImageURL}
+            borderRadius='full'
+            boxSize='18px'
+            mr={2}
+            alt={displayText ? `${displayText} icon` : ''}
+          />
         )}
+        {!ImageURL && <Icon as={icon} fontSize={20} mr={2} color={iconColor} />}
         {displayText}
       </Flex>
     </MenuItem>
