622bb76cccf7778e9e2532709292fa0327f121bc
Mario Ballesteros <mariob360@gmail.com>
2023-08-08 14:51:06 -0700

    Commit: Limit User Post Votes Query to First 10 Post IDs
    - Modified the `useUserPostVotes` hook to handle only the first 10 post IDs due to query constraints in Firestore.
    - Added a slice method to restrict the post IDs to the first 10.
    - Included comments outlining a plan for future enhancements to manage batching and performance considerations.

--- src/hooks/useUserPostVotes.tsx
+++ src/hooks/useUserPostVotes.tsx
@@ -4,24 +4,31 @@ import { User } from 'firebase/auth';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 
+// This hook handles only the first 10 post IDs due to query constraints.
+// Future Enhancement:
+// 1. Divide post IDs into batches of 10.
+// 2. Query each batch to fetch post votes.
+// 3. Combine all results into one array.
+// Use Promise.all for async handling, and consider performance and loading times.
+
 const useUserPostVotes = (user: User | null | undefined, postStateValue: PostState) => {
   const [postVotes, setPostVotes] = useState<PostVote[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   const userUid = user?.uid;
-  const postIDs = postStateValue.posts.map((post) => post.id);
+  const postIDs = postStateValue.posts.map((post) => post.id).slice(0, 10);
 
   useEffect(() => {
     const fetchUserPostVotes = async () => {
       setLoading(true);
       setError(null);
 
-      try {
-        if (postIDs.length === 0 || !userUid) {
-          return;
-        }
+      if (postIDs.length === 0 || !userUid) {
+        return;
+      }
 
+      try {
         const postVotesCollection = collection(firestore, `users/${userUid}/postVotes`);
         const postVotesFilter = where('postId', 'in', postIDs);
         const postVotesQuery = query(postVotesCollection, postVotesFilter);
