221eda2bd43040d0847075d5bda0939ca604b373
Mario Ballesteros <mariob360@gmail.com>
2023-08-14 17:52:43 -0700

    refactor: Enhance useAuthCommunityPosts with structured code
    - Introduced an interface for the return type
    - Extracted fetch logic into 'fetchCommunityPostsFromFirestore' function
    - Added a utility function 'handleFetchError' for error handling
    - Utilized 'toPost' utility function for post conversion
    - Replaced the useEffect dependency with the newly created useCallback

--- src/hooks/useAuthCommunityPosts.tsx
+++ src/hooks/useAuthCommunityPosts.tsx
@@ -1,53 +1,69 @@
 import { Post } from '@/atoms/postsAtoms';
 import { firestore } from '@/firebase/clientApp';
 import { collection, getDocs, query, where } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import useCommunityData from './useCommunityData';
 
-const UseAuthCommunityPosts = () => {
+import { toPost } from '@/utils/convertToPost';
+
+interface useAuthPostResult {
+  authPosts: Post[];
+  loading: boolean;
+  error: string | null;
+}
+
+// Fetches community posts from Firestore based on given community IDs
+const fetchCommunityPostsFromFirestore = async (communityIds: string[]): Promise<Post[]> => {
+  const postsCollection = collection(firestore, 'posts');
+  const postVotesFilter = where('communityId', 'in', communityIds);
+  const postsQuery = query(postsCollection, postVotesFilter);
+  const postDocs = await getDocs(postsQuery);
+  return postDocs.docs.map(toPost);
+};
+
+// Handles errors during the fetch operation, returning a user-friendly error message
+const handleFetchError = (error: unknown): string => {
+  if (error instanceof Error) {
+    return error.message;
+  }
+  return 'An unknown error occurred.';
+};
+
+const useAuthCommunityPosts = (): useAuthPostResult => {
   const { communityStateValue } = useCommunityData();
 
   const [authPosts, setAuthPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchCommunityPosts = async () => {
-      setLoading(true);
-      setError(null);
+  const fetchCommunityPosts = useCallback(async () => {
+    if (communityStateValue.mySnippets.length === 0) {
+      return;
+    }
 
-      if (communityStateValue.mySnippets.length === 0) {
-        return;
-      }
+    setLoading(true);
+    setError(null);
 
-      try {
-        const userCommunityIDs = communityStateValue.mySnippets.map((snippet) => snippet.communityId);
-        const postsCollection = collection(firestore, 'posts');
-        const postVotesFilter = where('communityId', 'in', userCommunityIDs);
-        const postsQuery = query(postsCollection, postVotesFilter);
-
-        const postDocs = await getDocs(postsQuery);
-
-        const fetchedPosts = postDocs.docs.map((doc) => {
-          return { id: doc.id, ...doc.data() };
-        });
-
-        setAuthPosts(fetchedPosts as Post[]);
-      } catch (error: unknown) {
-        if (error instanceof Error) {
-          setError(error.message);
-        } else {
-          setError('An unknown error occurred.');
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchCommunityPosts();
+    try {
+      const userCommunityIDs = communityStateValue.mySnippets.map((snippet) => snippet.communityId);
+      const fetchedPosts = await fetchCommunityPostsFromFirestore(userCommunityIDs);
+      setAuthPosts(fetchedPosts);
+    } catch (error: unknown) {
+      setError(handleFetchError(error));
+    } finally {
+      setLoading(false);
+    }
   }, [communityStateValue.mySnippets]);
 
-  return { authPosts, loading, error };
+  useEffect(() => {
+    fetchCommunityPosts();
+  }, [fetchCommunityPosts]);
+
+  return {
+    authPosts,
+    loading,
+    error,
+  };
 };
 
-export default UseAuthCommunityPosts;
+export default useAuthCommunityPosts;
