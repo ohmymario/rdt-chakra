52b4a1b72484ee3dc6d73edb7408f59f599a3ddb
Mario Ballesteros <mariob360@gmail.com>
2023-08-30 13:20:04 -0700

    Refactor useImageUpload hook and integrate useSelectFile for better error handling
    - Replaced `useSelecfile` with the updated `useSelectFile` to manage file selection.
    - Introduced `errorMessage` and `removeSelectedFile` methods from `useSelectFile` for better error handling and file removal.
    - Abstracted the image upload logic into a separate function `uploadImageToStorage` for better code readability and separation of concerns.
    - Added type annotation for `uploadingImage` state variable for better type safety.

--- src/hooks/useImageUpload.tsx
+++ src/hooks/useImageUpload.tsx
@@ -1,6 +1,6 @@
 import { Community } from '@/atoms/communitiesAtom';
 import { firestore, storage } from '@/firebase/clientApp';
-import useSelecfile from '@/hooks/useSelectFile';
+import useSelectFile from '@/hooks/useSelectFile';
 import { doc, updateDoc } from 'firebase/firestore';
 import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 import { useRef, useState } from 'react';
@@ -8,27 +8,29 @@ import { useRef, useState } from 'react';
 export const useImageUpload = (communityData: Community) => {
   // Image Upload / Input Ref
   const selectedFileRef = useRef<HTMLInputElement>(null);
-  const { selectedFile, onSelectFile, setSelectedFile } = useSelecfile();
-  const [uploadingImage, setUploadingImage] = useState(false);
+  const { selectedFile, onSelectFile, errorMessage, removeSelectedFile } = useSelectFile();
+  const [uploadingImage, setUploadingImage] = useState<boolean>(false);
 
   const { id } = communityData;
+
+  const uploadImageToStorage = async (selectedFile: string) => {
+    const imageLocation = `communities/${id}/image`;
+    const imageRef = ref(storage, imageLocation);
+    await uploadString(imageRef, selectedFile, 'data_url');
+    const downloadURL = await getDownloadURL(imageRef);
+    await updateDoc(doc(firestore, 'communities', id), {
+      imageURL: downloadURL,
+    });
+  };
 
   const onUpdateImage = async () => {
     if (!selectedFile) return;
     setUploadingImage(true);
 
     try {
-      const imageLocation = `communities/${id}/image`;
-      const imageRef = ref(storage, imageLocation);
-      await uploadString(imageRef, selectedFile, 'data_url');
-      const downloadURL = await getDownloadURL(imageRef);
-      await updateDoc(doc(firestore, 'communities', id), {
-        imageURL: downloadURL,
-      });
-
-      // Operation Complete
+      await uploadImageToStorage(selectedFile);
       setUploadingImage(false);
-      setSelectedFile(null);
+      removeSelectedFile();
     } catch (error) {
       setUploadingImage(false);
       console.error(error);
