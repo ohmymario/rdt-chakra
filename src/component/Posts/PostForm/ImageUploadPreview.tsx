545201664fc8882c1f15de3b474615c49ffcb1f3
Mario Ballesteros <mariob360@gmail.com>
2023-09-07 14:39:46 -0700

    feat: Add ImageUploadPreview component
    - Implemented image preview functionality
    - Added Back to Post and Remove buttons
    - Integrated with NewPostForm via tabLabels

---
+++ src/component/Posts/PostForm/ImageUploadPreview.tsx
@@ -0,0 +1,28 @@
+import { Button, Image, Spacer, Stack } from '@chakra-ui/react';
+import { tabLabels } from '../NewPostForm';
+
+interface ImageUploadPreviewProps {
+  selectedFile: string;
+  setActiveTab: (tab: tabLabels) => void;
+  resetSelectedFile: () => void;
+}
+
+const ImageUploadPreview = (props: ImageUploadPreviewProps) => {
+  const { selectedFile, setActiveTab, resetSelectedFile } = props;
+  return (
+    <>
+      <Image src={selectedFile} alt='User selected image for upload' maxW='400px' maxH='400px' />
+      <Spacer />
+      <Stack direction='row' spacing={4} mt={6}>
+        <Button height='28px' aria-label='Go back to post editing' onClick={() => setActiveTab('Post')}>
+          Back to Post
+        </Button>
+        <Button height='28px' variant='outline' aria-label='Remove selected image' onClick={resetSelectedFile}>
+          Remove
+        </Button>
+      </Stack>
+    </>
+  );
+};
+
+export default ImageUploadPreview;
