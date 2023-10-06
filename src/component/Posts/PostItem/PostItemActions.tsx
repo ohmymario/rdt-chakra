a56624fc428e3984dc56ed316f8d982035b34a4e
Mario Ballesteros <mariob360@gmail.com>
2023-10-05 18:29:02 -0700

    feat: move actions logic
    - action logic separated to another component
    - typescript interface created for each prop

--- src/component/Posts/PostItem/PostItemActions.tsx
+++ src/component/Posts/PostItem/PostItemActions.tsx
@@ -1,4 +1,6 @@
 import { Flex, HStack, Icon, Spinner, Text } from '@chakra-ui/react';
+
+// React Icons
 import { AiOutlineDelete } from 'react-icons/ai';
 import { BsChat } from 'react-icons/bs';
 import { IoArrowRedoOutline, IoBookmarkOutline } from 'react-icons/io5';
@@ -11,12 +13,28 @@ interface PostItemActionsProps {
   handleDelete: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => Promise<void>;
 }
 
+interface ActionItemProps {
+  icon: React.ElementType;
+  text: string;
+  action?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => Promise<void>;
+}
+
 const actionStyles = {
   align: 'center',
   gap: 1.5,
   px: 1.5,
   py: 2,
   _hover: { bg: 'gray.200' },
+const ActionItem = (props: ActionItemProps) => {
+  const { icon, text, action } = props;
+  return (
+    <Flex {...actionStyles} onClick={action}>
+      <Icon as={icon} fontSize={20} />
+      <Text>{text}</Text>
+    </Flex>
+  );
+};
+
 };
 
 const PostItemActions = (props: PostItemActionsProps) => {
@@ -24,6 +42,10 @@ const PostItemActions = (props: PostItemActionsProps) => {
 
   return (
     <HStack fontSize='xs' spacing={1} fontWeight={600} color='gray.500'>
+      <ActionItem icon={BsChat} text={`${numberOfComments} Comments`} />
+      <ActionItem icon={SlPresent} text='Award' />
+      <ActionItem icon={IoArrowRedoOutline} text='Share' />
+      <ActionItem icon={IoBookmarkOutline} text='Save' />
       <Flex {...actionStyles}>
         <Icon as={BsChat} fontSize={20} />
         <Text>{numberOfComments} Comments</Text>
