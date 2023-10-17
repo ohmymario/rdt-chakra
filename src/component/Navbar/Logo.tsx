ffc6055ab0548d550156fea36092074440aad001
Mario Ballesteros <mariob360@gmail.com>
2023-10-16 17:14:12 -0700

    feature : clean imports and click function
    - add comments to better highlight different imports
    - introduce ability to change menu to home
    - clicking the component direct the user to the homepage

--- src/component/Navbar/Logo.tsx
+++ src/component/Navbar/Logo.tsx
@@ -1,8 +1,22 @@
-import { FunctionComponent } from 'react';
+// React & Next.js core
+import { Flex, Image } from '@chakra-ui/react';
+
+// Atoms
+import { defaultMenuItem } from '@/atoms/directoryMenuAtom';
+
+// Custom Hooks
+import useDirectory from '@/hooks/useDirectory';
 
 interface LogoProps {}
 
 const Logo = (props: LogoProps) => {
+  const { onSelectMenuItem } = useDirectory();
+
+  const handleHomeClick = () => {
+    // changes the Directory menu to Home
+    onSelectMenuItem(defaultMenuItem);
+  };
+
   return (
     <Flex align='center' cursor='pointer' onClick={handleHomeClick}>
       <Image src='/images/redditFace.svg' alt='logo' height='30px' />
