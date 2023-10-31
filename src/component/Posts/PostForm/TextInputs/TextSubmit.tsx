84ecdf48177cbb82fb16706f8c0af49807d66872
Mario Ballesteros <mariob360@gmail.com>
2023-10-30 19:03:25 -0700

    feat: begin movement of post button
    - new component named TextSubmit

---
+++ src/component/Posts/PostForm/TextInputs/TextSubmit.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from 'react'
+
+interface TextSubmitProps {}
+
+const TextSubmit = (props: TextSubmitProps) => {
+  return (
+    <div>TextSubmit</div>
+  )
+}
+
+export default TextSubmit
+
