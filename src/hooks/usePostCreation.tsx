47a72a466348aa6ddd5b06b47fb2c20bf1b62016
Mario Ballesteros <mariob360@gmail.com>
2024-01-05 16:08:00 -0800

    feat: move post creation
    - new hook created to handle post creation

---
+++ src/hooks/usePostCreation.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { firestore } from '@/firebase/clientApp';
+import { addDoc, collection, serverTimestamp, DocumentReference, Timestamp } from 'firebase/firestore';
+import { User } from 'firebase/auth';
+
+interface TextInputs {
+  title: string;
+  body: string;
+}
+
+/**
+ * @desc Manages the creation of a new post.
+ * @param user The current logged in user.
+ * @param communityImageURL The image URL of the community.
+ * @param selectedFile The selected file for image upload.
+ * @param onUploadImage Function to upload an image.
+ * @returns An object containing functions and states related to post creation.
+ */
+
+export const usePostCreation = (
+  user: User,
+  communityImageURL: string,
+  selectedFile: string,
+  onUploadImage: (docRef: DocumentReference) => Promise<void>
+) => {
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+  const [loadingState, setLoadingState] = useState<boolean>(false);
+
+  const createPostObject = (textInput: TextInputs) => {
+    const { communityId } = router.query;
+    const { title, body } = textInput;
+    const { uid, email } = user;
+
+    const newPost = {
+      communityId: communityId as string,
+      communityImageURL: communityImageURL || '',
+      creatorId: uid,
+      creatorDisplayName: email!.split('@')[0],
+      title: title,
+      body: body,
+      numberOfComments: 0,
+      voteStatus: 1,
+      createdAt: serverTimestamp() as Timestamp,
+    };
+
+    return newPost;
+  };
+
+  /**
+   * Creates a new post object and adds it to the database.
+   * @param textInput Object containing title and body of the post.
+   */
+
+  const createPost = async (textInput: TextInputs): Promise<void> => {
+    setLoadingState(true);
+    setError(null);
+
+    try {
+      const newPost = createPostObject(textInput);
+      const postDocRef = await addDoc(collection(firestore, 'posts'), newPost);
+
+      if (selectedFile) {
+        await onUploadImage(postDocRef);
+      }
+
+      // Redirect or perform further actions after successful post creation
+      router.push(`/some/redirect/path`);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(`Error creating post: ${error.message}`);
+      } else {
+        setError('An unexpected error occurred. Please try again.');
+      }
+    } finally {
+      setLoadingState(false);
+    }
+  };
+
+  return { createPost, error, setError, loadingState };
+};
--- src/hooks/useTabState.tsx
+++ src/hooks/useTabState.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 
 /**
  * @desc Manages the state for active tab in the New Post Form.
- * @param {string} initialTab - The initial tab to be active.
- * @returns {Object} An object containing the active tab and a setter function.
+ * @param - The initial tab to be active.
+ * @returns An object containing the active tab and a setter function.
  */
 
 export type tabLabels = 'Post' | 'Image & Video' | 'Link' | 'Poll' | 'Talk';
