23a04f29de5493bc192668d0ddf472a51e563897
Mario Ballesteros <mariob360@gmail.com>
2024-01-16 15:51:53 -0800

    style: capitalize interface
    - inputType to InputType

--- src/hooks/useTextInput.tsx
+++ src/hooks/useTextInput.tsx
@@ -1,12 +1,22 @@
 import { useState } from 'react';
 
-export interface inputType {
+export interface InputType {
   title: string;
   body: string;
 }
 
+/**
+ * @desc Manages the text input state for a form.
+ * @returns An object containing the text input state, a state setter function, and an input change handler.
+ */
+
 export const useTextInput = () => {
-  const [textInput, setTextInput] = useState<inputType>({ title: '', body: '' });
+  const [textInput, setTextInput] = useState<InputType>({ title: '', body: '' });
+
+  /**
+   * @desc Handles changes to text input fields and updates the state.
+   * @param e - The React change event from the input fields.
+   */
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
     const { name, value } = e.target;
