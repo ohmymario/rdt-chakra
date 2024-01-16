81348ce960c769d5a49d1aeba4411bdbce6c4a65
Mario Ballesteros <mariob360@gmail.com>
2024-01-16 15:51:29 -0800

    feat: add handleInputChange
    - add method to update state to avoid errors from improper update

--- src/hooks/useTextInput.tsx
+++ src/hooks/useTextInput.tsx
@@ -8,5 +8,10 @@ export interface inputType {
 export const useTextInput = () => {
   const [textInput, setTextInput] = useState<inputType>({ title: '', body: '' });
 
-  return { textInput, setTextInput };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    const { name, value } = e.target;
+    setTextInput((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return { textInput, setTextInput, handleInputChange };
 };
