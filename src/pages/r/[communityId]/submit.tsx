c994f8b29ccd33d0c4aae56e619ebfd062241da2
Mario Ballesteros <mariob360@gmail.com>
2024-02-06 11:35:28 -0800

    chore: clean up imports
    - add comments to imports
    - comments sort and separate the imports

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,11 +1,18 @@
+// Libs
+import { Box, Text, VStack } from '@chakra-ui/react';
+import { FunctionComponent } from 'react';
+
+// Components
 import About from '@/component/Community/About/About';
 import PageContent from '@/component/Layout/PageContent';
 import NewPostForm from '@/component/Posts/NewPostForm';
+
+// Firebase
 import { auth } from '@/firebase/clientApp';
-import useCommunityData from '@/hooks/useCommunityData';
-import { Box, Text, VStack } from '@chakra-ui/react';
-import { FunctionComponent } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+
+// Hooks
+import useCommunityData from '@/hooks/useCommunityData';
 
 const SubmitPostPage: FunctionComponent = (props) => {
   const [user] = useAuthState(auth);
