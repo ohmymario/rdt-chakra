346035d4da3ed17d9f417c31d890aa86b1f887d1
Mario Ballesteros <mariob360@gmail.com>
2024-02-26 22:36:15 -0800

    style: add styles for draft counter
    - spaced text and draft counter
    - increase font weight on "Create a Post"
    - add comment reminding to change the sidebar

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,11 +1,12 @@
 // Libs
-import { Box, Text, VStack } from '@chakra-ui/react';
+import { Box, Flex, HStack, Text, VStack } from '@chakra-ui/react';
 import { FunctionComponent } from 'react';
 
 // Components
 import About from '@/component/Community/About/About';
 import PageContent from '@/component/Layout/PageContent';
 import NewPostForm from '@/component/Posts/NewPostForm';
+import PostFormDraftCounter from '@/component/Posts/PostForm/PostFormTitle/PostFormDraftCounter';
 
 // Firebase
 import { auth } from '@/firebase/clientApp';
@@ -20,11 +21,23 @@ const SubmitPostPage: FunctionComponent = (props) => {
   return (
     <PageContent>
       <VStack w='100%'>
-        <Box borderBottom='1px solid white' w='100%' p='14px 0' mb='8px'>
-          <Text>Create a post</Text>
-        </Box>
+        <HStack
+          borderBottom='1px solid white'
+          w='100%'
+          p='14px 0'
+          mb='8px'
+          justifyContent={'space-between'}
+          paddingRight='20px'
+        >
+          <Text fontWeight={'600'}>Create a post</Text>
+
+          {/* DRAFTS COUNTER */}
+          <PostFormDraftCounter />
+        </HStack>
         {user && <NewPostForm communityImageURL={communityStateValue.currentCommunity?.imageURL} />}
       </VStack>
+
+      {/* todo: change to rules */}
       <>{communityStateValue.currentCommunity && <About communityData={communityStateValue.currentCommunity} />}</>
     </PageContent>
   );
