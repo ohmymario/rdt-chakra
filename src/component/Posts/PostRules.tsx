9efd68cd94c7b71ce1cd719a2b3b5056f6a0eed1
Mario Ballesteros <mariob360@gmail.com>
2024-03-08 13:13:19 -0800

    refactor: Enhance PostRules component for readability and modularity
    - Introduced `rules` array to dynamically generate rule list items, improving maintainability.
    - Extracted common styles for list and list items into `listRulesStyle` and `listRuleStyle` objects, adhering to DRY principles and enhancing readability.
    - Replaced static text elements with a map function over `rules` array for scalable rule additions.
    - Updated layout structure with `Flex` and `Box` components for a more flexible and responsive design.
    - Enhanced accessibility by adding alt text to the Reddit rules icon image.
    - Adjusted styling details such as margins, padding, and border properties to match updated design specifications.
    - Incorporated `Heading` component for semantic structure and improved typography.

--- src/component/Posts/PostRules.tsx
+++ src/component/Posts/PostRules.tsx
@@ -1,40 +1,55 @@
-import { Flex, Heading, Image, ListItem, OrderedList, Text } from '@chakra-ui/react';
+import { Flex, Image, ListItem, OrderedList, Text, Box, Heading, ListItemProps, FlexProps } from '@chakra-ui/react';
 
-interface PostRulesProps {}
+const PostRules = () => {
+  const rules = [
+    'Remember the human',
+    'Behave like you would in real life',
+    'Look for the original source of content',
+    'Search for duplicates before posting',
+    "Read the community's rules",
+  ];
 
-const PostRules = (props: PostRulesProps) => {
+  const listRulesStyle: FlexProps = {
+    direction: 'column',
+    bg: 'white',
+    padding: '12px 17px',
+    borderRadius: '4px',
+    marginTop: '27px',
+    marginBottom: '5px',
+    border: '1px solid',
+    borderColor: 'gray.200',
+  };
+
+  const listRuleStyle: ListItemProps = {
+    listStylePos: 'inside',
+    borderBottom: '1px solid',
+    borderColor: 'gray.200',
+    padding: '10px 5px 5px 0',
+  };
+
   return (
-    <Flex direction='column'>
-      {/* 5 Rules */}
-      <Flex direction='column' bg='white' padding='12px 17px' borderRadius='4px' marginTop='27px'>
-        {/* Image goes here */}
-
-        <Flex align='center' gap='5px' borderBottom='1px solid' borderColor='gray.200' pb='5px' border='1px solid'>
-          <Image src='/images/redditRules.svg' alt='' height='36px' width='39px' />
-          <Text>Posting to Reddit</Text>
+    <Flex direction='column' minWidth='315px'>
+      <Flex {...listRulesStyle}>
+        {/* Header & Image */}
+        <Flex align='center' gap='5px' borderBottom='1px solid' borderColor='gray.200' pb='5px'>
+          <Image src='/images/redditRules.svg' alt='Reddit rules icon' height='36px' width='39px' />
+          <Heading as='h5' size='s' fontWeight='500'>
+            Posting to Reddit
+          </Heading>
         </Flex>
 
-        <OrderedList m='0' fontSize='14px' border='1px solid'>
-          <ListItem borderBottom='1px solid' borderColor='gray.200' padding='10px 5px 5px 0px' listStylePos='inside'>
-            Remember the human
-          </ListItem>
-          <ListItem borderBottom='1px solid' borderColor='gray.200' padding='10px 5px 5px 0px' listStylePos='inside'>
-            Behave like you would in real life
-          </ListItem>
-          <ListItem borderBottom='1px solid' borderColor='gray.200' padding='10px 5px 5px 0px' listStylePos='inside'>
-            Look for the original source of content
-          </ListItem>
-          <ListItem borderBottom='1px solid' borderColor='gray.200' padding='10px 5px 5px 0px' listStylePos='inside'>
-            Search for duplicates before posting
-          </ListItem>
-          <ListItem borderBottom='1px solid' borderColor='gray.200' padding='10px 5px 5px 0px' listStylePos='inside'>
-            Read the community\u2019s rules
-          </ListItem>
+        {/* Rules List */}
+        <OrderedList m='0' fontSize='14px'>
+          {rules.map((rule, i) => (
+            <ListItem {...listRuleStyle} key={i}>
+              {rule}
+            </ListItem>
+          ))}
         </OrderedList>
       </Flex>
 
-      {/* Content Policy */}
-      <Text fontSize='12px' color='gray.500' marginY='10px' lineHeight='16px' maxWidth='250px'>
+      {/* Content Policy Reminder */}
+      <Text fontSize='12px' color='gray.500' my='10px' lineHeight='16px' maxWidth='250px'>
         Please be mindful of reddit&apos;s{' '}
         <Text as='span' color='blue.300' _hover={{ cursor: 'pointer' }}>
           content policy
