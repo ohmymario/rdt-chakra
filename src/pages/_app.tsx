3543c3cdef37c2da57e98ba0891ee28d6745dcb0
Mario Ballesteros <mariob360@gmail.com>
2024-03-14 20:46:01 -0700

    chore: update imports syntax

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 // import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
-import { theme } from "../chakra/theme";
-import Layout from "@/component/Layout/Layout";
-import { RecoilRoot } from "recoil";
+import { theme } from '../chakra/theme';
+import Layout from '@/component/Layout/Layout';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
