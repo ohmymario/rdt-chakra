7892cefb7a6b0f4e2fbe98ced6a8e4c2bb9572e7
Mario Ballesteros <mariob360@gmail.com>
2024-04-11 13:24:56 -0700

    refactor: move SortOptions data to a separate file

---
+++ src/component/Community/Sorting/SortOptions.ts
@@ -0,0 +1,35 @@
+import { RiRocket2Fill } from 'react-icons/ri';
+import { PiFlame } from 'react-icons/pi';
+import { FaRegSun } from 'react-icons/fa';
+import { FaEllipsisH } from 'react-icons/fa';
+import { FaArrowUpWideShort } from 'react-icons/fa6';
+import { IconType } from 'react-icons';
+
+interface SortingOption {
+  Text?: string;
+  Icon: IconType;
+}
+
+const sortOptions: SortingOption[] = [
+  {
+    Text: 'Best',
+    Icon: RiRocket2Fill,
+  },
+  {
+    Text: 'Hot',
+    Icon: PiFlame,
+  },
+  {
+    Text: 'New',
+    Icon: FaRegSun,
+  },
+  {
+    Text: 'Top',
+    Icon: FaArrowUpWideShort,
+  },
+  {
+    Icon: FaEllipsisH,
+  },
+];
+
+export default sortOptions;
