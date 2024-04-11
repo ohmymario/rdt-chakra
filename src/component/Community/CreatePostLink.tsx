88865cff73c33f3144bdf67477a28f88edc6252f
Mario Ballesteros <mariob360@gmail.com>
2024-04-11 14:14:03 -0700

    styles : move styles outside component

--- src/component/Community/CreatePostLink.tsx
+++ src/component/Community/CreatePostLink.tsx
@@ -18,7 +18,40 @@ import { authModalState } from '@/atoms/authModalAtom';
 // Hooks
 import useDirectory from '@/hooks/useDirectory';
 
-const CreatePostLink: FunctionComponent = () => {
+const flexContainerStyles = {
+  flex: 1,
+  justifyContent: 'space-evenly',
+  alignItems: 'center',
+  bg: 'white',
+  height: '56px',
+  borderRadius: 4,
+  border: '1px solid',
+  borderColor: 'gray.300',
+  p: 2,
+};
+
+const inputStyles = {
+  placeholder: 'Create Post',
+  fontSize: '10pt',
+  bg: 'gray.50',
+  mr: 4,
+  borderColor: 'gray.200',
+  borderRadius: 4,
+  height: '36px',
+  __placeholder: {
+    bg: 'white',
+    border: '1px solid',
+    borderColor: 'blue.500',
+  },
+  __focus: {
+    outline: 'none',
+    bg: 'white',
+    border: '1px solid',
+    borderColor: 'blue.500',
+  },
+};
+
+const CreatePostLink = () => {
   const router = useRouter();
   const [user] = useAuthState(auth);
   const setAuthModalState = useSetRecoilState(authModalState);
@@ -45,42 +78,8 @@ const CreatePostLink: FunctionComponent = () => {
     toggleMenuOpen();
   };
 
-  const flexContainerStyles = {
-    flex: 1,
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    bg: 'white',
-    height: '56px',
-    borderRadius: 4,
-    border: '1px solid',
-    borderColor: 'gray.300',
-    p: 2,
-  };
-
-  const inputStyles = {
-    placeholder: 'Create Post',
-    fontSize: '10pt',
-    bg: 'gray.50',
-    mr: 4,
-    borderColor: 'gray.200',
-    borderRadius: 4,
-    height: '36px',
-    __placeholder: {
-      bg: 'white',
-      border: '1px solid',
-      borderColor: 'blue.500',
-    },
-    __focus: {
-      outline: 'none',
-      bg: 'white',
-      border: '1px solid',
-      borderColor: 'blue.500',
-    },
-  };
-
   return (
     <Flex {...flexContainerStyles}>
-      {/* Reddit Icon */}
       <Icon as={FaReddit} fontSize={36} color='gray.300' mr={4} />
       <Input {...inputStyles} onClick={() => clickHandler()} />
       <Icon as={IoImageOutline} fontSize={24} mr={4} color='gray.400' cursor='pointer' />
